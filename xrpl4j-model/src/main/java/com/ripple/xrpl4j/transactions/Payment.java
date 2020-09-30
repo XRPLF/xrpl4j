4dfc335c3eeda61a6928ad95e164ad5a1b7980a8
nkramer44 <noah.ph.kramer@gmail.com>
2020-09-30 12:44:55 -0400

    javadoc Payment

--- xrpl4j-model/src/main/java/com/ripple/xrpl4j/transactions/Payment.java
+++ xrpl4j-model/src/main/java/com/ripple/xrpl4j/transactions/Payment.java
@@ -42,88 +42,206 @@ public interface Payment {
     return TransactionType.PAYMENT;
   }
 
-
+  /**
+   * The {@link String} representation of an integer amount of XRP, in drops, to be destroyed as a cost for
+   * distributing this Payment transaction to the network.
+   *
+   * This field is auto-fillable
+   * @see "https://xrpl.org/transaction-common-fields.html#auto-fillable-fields"
+   */
   @JsonProperty("Fee")
-  Optional<String> fee();
+  String fee();
 
+  /**
+   * The sequence number of the account sending the {@link Payment}. A {@link Payment} is only valid if the Sequence
+   * number is exactly 1 greater than the previous transaction from the same account.
+   *
+   * This field is auto-fillable
+   * @see "https://xrpl.org/transaction-common-fields.html#auto-fillable-fields"
+   */
   @JsonProperty("Sequence")
-  Optional<UnsignedInteger> sequence();
+  UnsignedInteger sequence();
 
+  /**
+   * Hash value identifying another transaction. If provided, this {@link Payment} is only valid if the sending
+   * account's previously-sent transaction matches the provided hash.
+   */
   @JsonProperty("AccountTxnID")
   Optional<Hash256> accountTransactionId();
 
+  /**
+   * Set of {@link Flags} for this {@link Payment}, which have been properly combined to yield a {@link Flags} object
+   * containing the {@link Long} representation of the set bits.
+   *
+   * This field can either be set manually, or can be derived by setting {@link Payment#tfFullyCanonicalSig()},
+   * {@link Payment#tfNoDirectRipple()}, {@link Payment#tfPartialPayment()}, and {@link Payment#tfLimitQuality()}
+   */
   @JsonProperty("Flags")
   Optional<Flags> flags();
 
+  /**
+   * Highest ledger index this transaction can appear in. Specifying this field places a strict upper limit
+   * on how long the transaction can wait to be validated or rejected.
+   */
   @JsonProperty("LastLedgerSequence")
   Optional<UnsignedInteger> lastLedgerSequence();
 
+  /**
+   * Additional arbitrary information used to identify this {@link Payment}.
+   */
   @JsonProperty("Memos")
   List<MemoWrapper> memos();
 
+  /**
+   * Array of {@link SignerWrapper}s that represent a multi-signature which authorizes this {@link Payment}.
+   */
   @JsonProperty("Signers")
   List<SignerWrapper> signers();
 
+  /**
+   * Arbitrary {@link UnsignedInteger} used to identify the reason for this {@link Payment}, or a sender on whose
+   * behalf this {@link Payment} is made.
+   */
   @JsonProperty("SourceTag")
   Optional<UnsignedInteger> sourceTag();
 
+  /**
+   * Hex representation of the public key that corresponds to the private key used to sign this transaction.
+   * If an empty string, indicates a multi-signature is present in the {@link Payment#signers()} field instead.
+   *
+   * This field is automatically added when signing this {@link Payment}.
+   */
   @JsonProperty("SigningPubKey")
   Optional<String> signingPublicKey();
 
+  /**
+   * The signature that verifies this transaction as originating from the account it says it is from.
+   *
+   * This field is automatically added when signing this {@link Payment}.
+   */
   @JsonProperty("TxnSignature")
   Optional<String> transactionSignature();
 
+  /**
+   * The amount of currency to deliver. If the {@link Payment#tfPartialPayment()} flag is set, deliver up to
+   * this amount instead.
+   */
   @JsonProperty("Amount")
   CurrencyAmount amount();
 
+  /**
+   * The unique {@link Address} of the account receiving the payment.
+   */
   @JsonProperty("Destination")
   Address destination();
 
+  /**
+   * Arbitrary {@link UnsignedInteger} tag that identifies the reason for the payment to the destination,
+   * or a hosted recipient to pay.
+   */
   @JsonProperty("DestinationTag")
   Optional<UnsignedInteger> destinationTag();
 
+  /**
+   * Arbitrary 256-bit hash representing a specific reason or identifier for this payment.
+   */
   @JsonProperty("InvoiceID")
   Optional<Hash256> invoiceId();
 
+  /**
+   * A {@link List} of {@link List}s of payment paths to be used for this transaction.
+   * Must be omitted for XRP-to-XRP transactions.
+   *
+   * This field is auto-fillable
+   * @see "https://xrpl.org/transaction-common-fields.html#auto-fillable-fields"
+   */
   @JsonProperty("Paths")
   List<List<Path>> paths();
 
+  /**
+   * Highest amount of source currency this transaction is allowed to cost, including transfer fees,
+   * exchange rates, and slippage. Does not include the XRP destroyed as a cost for submitting the transaction.
+   *
+   * Must be supplied for cross-currency/cross-issue payments. Must be omitted for XRP-to-XRP payments.
+   *
+   * @see "https://xrpl.org/transfer-fees.html"
+   * @see "https://en.wikipedia.org/wiki/Slippage_%28finance%29"
+   */
   @JsonProperty("SendMax")
   Optional<CurrencyAmount> sendMax();
 
+  /**
+   * Minimum amount of destination currency this {@link Payment} should deliver. Only valid if this the
+   * {@link Payment#tfPartialPayment()} flag is set.
+   */
   @JsonProperty("DeliverMin")
   Optional<CurrencyAmount> deliverMin();
 
+  /**
+   * Flags indicating that a fully-canonical signature is required.
+   * This flag is highly recommended.
+   *
+   * @see "https://xrpl.org/transaction-common-fields.html#flags-field"
+   */
   @JsonIgnore
   @Value.Default
   default boolean tfFullyCanonicalSig() {
     return true;
-  };
+  }
 
+  /**
+   * Flag indicated to only use paths included in the {@link Payment#paths()} field.
+   * This is intended to force the transaction to take arbitrage opportunities. Most clients do not need this.
+   */
   @JsonIgnore
   @Value.Default
   default boolean tfNoDirectRipple() {
     return false;
   }
 
+  /**
+   * If the specified {@link Payment#amount()} cannot be sent without spending more than {@link Payment#sendMax()},
+   * reduce the received amount instead of failing outright.
+   *
+   * @see "https://xrpl.org/partial-payments.html"
+   */
   @JsonIgnore
   @Value.Default
   default boolean tfPartialPayment() {
     return false;
   }
 
+  /**
+   * Only take paths where all the conversions have an input:output ratio that is equal or better than the ratio of
+   * {@link Payment#amount()}:{@link Payment#sendMax()}.
+   * @return
+   */
   @JsonIgnore
   @Value.Default
   default boolean tfLimitQuality() {
     return false;
   }
 
+  /**
+   * {@link Payment#flags()} must be constructed from the set of boolean flags in this transaction. However, because
+   * the individual flags are not present in the JSON representation of this {@link Payment}, when deserializing
+   * JSON to a {@link Payment}, we must be able to derive the boolean flags from the {@link Payment#flags()} field.
+   *
+   * The best way to do this with a {@link Value.Immutable} object like {@link Payment} is to normalize the object
+   * after it has been initially constructed.  Without this flag, there would be no way to determine if
+   * this normalization has already occurred, and an infinite recursive loop would occur during normalization.
+   */
   @JsonIgnore
   @Value.Default
   default boolean flagsConstructed() {
     return false;
   }
 
+  /**
+   * Constructs {@link Payment#flags()} from {@link Payment#tfFullyCanonicalSig()}, {@link Payment#tfNoDirectRipple()},
+   * {@link Payment#tfPartialPayment()}, and {@link Payment#tfLimitQuality()} if {@link Payment#flags()} is empty.
+   * Otherwise, the individual boolean flags are derived from {@link Payment#flags()}
+   */
   @Value.Check
   default Payment constructFlags() {
     // Avoid infinite recursion by tagging that the object has gone through this transformation

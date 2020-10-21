0817029be22feb98d7f3e4d1ebd3d8c78ba8f129
nkramer44 <noah.ph.kramer@gmail.com>
2020-10-21 10:34:45 -0400

    clean up Payment class

--- xrpl4j-model/src/main/java/com/ripple/xrpl4j/model/transactions/Payment.java
+++ xrpl4j-model/src/main/java/com/ripple/xrpl4j/model/transactions/Payment.java
@@ -20,7 +20,6 @@ import java.util.Optional;
 @Value.Immutable
 @JsonSerialize(as = ImmutablePayment.class)
 @JsonDeserialize(as = ImmutablePayment.class)
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 public interface Payment extends Transaction {
 
   static ImmutablePayment.Builder builder() {
@@ -128,60 +127,4 @@ public interface Payment extends Transaction {
     return flags().isSet(Flags.Payment.LIMIT_QUALITY);
   }
 
-  /**
-   * {@link Payment#flags()} must be constructed from the set of boolean flags in this transaction. However, because
-   * the individual flags are not present in the JSON representation of this {@link Payment}, when deserializing
-   * JSON to a {@link Payment}, we must be able to derive the boolean flags from the {@link Payment#flags()} field.
-   *
-   * The best way to do this with a {@link Value.Immutable} object like {@link Payment} is to normalize the object
-   * after it has been initially constructed.  Without this flag, there would be no way to determine if
-   * this normalization has already occurred, and an infinite recursive loop would occur during normalization.
-   */
-  /*@JsonIgnore
-  @Value.Default
-  default boolean flagsConstructed() {
-    return false;
-  }*/
-
-  /**
-   * Constructs {@link Payment#flags()} from {@link Payment#tfFullyCanonicalSig()}, {@link Payment#tfNoDirectRipple()},
-   * {@link Payment#tfPartialPayment()}, and {@link Payment#tfLimitQuality()} if {@link Payment#flags()} is empty.
-   * Otherwise, the individual boolean flags are derived from {@link Payment#flags()}
-   */
-  /*@Value.Check
-  default Payment constructFlags() {
-    // Avoid infinite recursion by tagging that the object has gone through this transformation
-    if (flagsConstructed()) {
-      return this;
-    }
-
-    // If flags() is null, then this Payment wasn't constructed from JSON, so we need to derive flags()
-    // from the boolean switches
-    if (!flags().isPresent()) {
-      Flags flags = (tfFullyCanonicalSig() ? Flags.Universal.FULLY_CANONICAL_SIG : Flags.UNSET)
-        .bitwiseOr((tfNoDirectRipple() ? Flags.Payment.NO_DIRECT_RIPPLE : Flags.UNSET)
-          .bitwiseOr(
-            (tfPartialPayment() ? Flags.Payment.PARTIAL_PAYMENT : Flags.UNSET))
-          .bitwiseOr(
-            (tfLimitQuality() ? Flags.Payment.LIMIT_QUALITY : Flags.UNSET)));
-
-      return Payment.builder()
-        .from(this)
-        .flags(flags)
-        .flagsConstructed(true)
-        .build();
-    }
-
-    // Otherwise, we know flags() from JSON, so we can derive the boolean switches from the flags
-    else {
-      return Payment.builder()
-        .from(this)
-        .tfFullyCanonicalSig(flags().get().isSet(Flags.Universal.FULLY_CANONICAL_SIG))
-        .tfNoDirectRipple(flags().get().isSet(Flags.Payment.NO_DIRECT_RIPPLE))
-        .tfPartialPayment(flags().get().isSet(Flags.Payment.PARTIAL_PAYMENT))
-        .tfLimitQuality(flags().get().isSet(Flags.Payment.LIMIT_QUALITY))
-        .flagsConstructed(true)
-        .build();
-    }
-  }*/
 }

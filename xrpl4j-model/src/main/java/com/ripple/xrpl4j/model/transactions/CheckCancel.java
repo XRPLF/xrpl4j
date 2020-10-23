61f9e8f4884bbb4490076adda50d355de77cc607
nkramer44 <noah.ph.kramer@gmail.com>
2020-10-23 15:45:18 -0400

    javadoc

--- xrpl4j-model/src/main/java/com/ripple/xrpl4j/model/transactions/CheckCancel.java
+++ xrpl4j-model/src/main/java/com/ripple/xrpl4j/model/transactions/CheckCancel.java
@@ -5,6 +5,11 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
 
+/**
+ * Cancels an unredeemed Check, removing it from the ledger without sending any money. The source or the
+ * destination of the check can cancel a Check at any time using this transaction type.
+ * If the Check has expired, any address can cancel it.
+ */
 @Value.Immutable
 @JsonSerialize(as = ImmutableCheckCancel.class)
 @JsonDeserialize(as = ImmutableCheckCancel.class)
@@ -21,6 +26,10 @@ public interface CheckCancel extends Transaction {
     return TransactionType.CHECK_CANCEL;
   }
 
+  /**
+   * The ID of the Check ledger object to cancel, as a 64-character hexadecimal string.
+   * @return A {@link Hash256} containing the ID of the Check ledger object in hexadecimal form.
+   */
   @JsonProperty("CheckID")
   Hash256 checkId();
 }

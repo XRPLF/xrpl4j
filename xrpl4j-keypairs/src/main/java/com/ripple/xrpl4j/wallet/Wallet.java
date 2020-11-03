1aaf599470fe4382be6fbea7f6b3ffc3fc18a406
nkramer44 <noah.ph.kramer@gmail.com>
2020-11-03 12:55:40 -0500

    get rid of todos

--- xrpl4j-keypairs/src/main/java/com/ripple/xrpl4j/wallet/Wallet.java
+++ xrpl4j-keypairs/src/main/java/com/ripple/xrpl4j/wallet/Wallet.java
@@ -18,19 +18,16 @@ public interface Wallet {
   /**
    * The private key of the wallet, encoded in hexadecimal.
    */
-  // TODO: Use wrapper type from Ed25519 lib?
   Optional<String> privateKey();
 
   /**
    * The public key of the wallet, encoded in hexadecimal.
    */
-  // TODO: Use wrapper type from Ed25519 lib?
   String publicKey();
 
   /**
    * The XRPL address of this wallet, in the Classic Address form.
    */
-  // TODO: Create wrapper type.
   Address classicAddress();
 
   /**

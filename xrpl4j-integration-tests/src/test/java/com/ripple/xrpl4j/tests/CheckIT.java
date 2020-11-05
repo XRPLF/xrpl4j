647995729ef7d900d6f87982255a06e7663e8e73
nkramer44 <noah.ph.kramer@gmail.com>
2020-11-05 16:11:08 -0500

    check cancel IT

--- xrpl4j-integration-tests/src/test/java/com/ripple/xrpl4j/tests/CheckIT.java
+++ xrpl4j-integration-tests/src/test/java/com/ripple/xrpl4j/tests/CheckIT.java
@@ -4,12 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.hash.Hashing;
 import com.google.common.primitives.UnsignedInteger;
+import com.ripple.xrpl4j.model.transactions.CheckCancel;
 import com.ripple.xrpl4j.model.transactions.CheckCash;
 import com.ripple.xrpl4j.model.transactions.CheckCreate;
 import com.ripple.xrpl4j.model.transactions.Hash256;
 import com.ripple.xrpl4j.model.transactions.XrpCurrencyAmount;
 import com.ripple.xrpl4j.wallet.Wallet;
 import com.ripple.xrplj4.client.model.accounts.AccountInfoResult;
+import com.ripple.xrplj4.client.model.accounts.AccountObjectsResult;
 import com.ripple.xrplj4.client.model.fees.FeeResult;
 import com.ripple.xrplj4.client.model.ledger.Check;
 import com.ripple.xrplj4.client.model.ledger.LedgerObject;
@@ -80,6 +82,124 @@ public class CheckIT extends AbstractIT {
           .plus(UnsignedInteger.valueOf(((XrpCurrencyAmount) check.sendMax()).value()))
           .minus(UnsignedInteger.valueOf(checkCash.fee().value())).toString());
     });
+
+    AccountObjectsResult accountObjectsResult = scanForAccountObjects(sourceWallet.classicAddress());
+    assertThat(accountObjectsResult.accountObjects()).matches(objects ->
+      objects.stream().noneMatch(findCheck(sourceWallet, destinationWallet, invoiceId))
+    );
+  }
+
+  @Test
+  public void createCheckAndSourceCancels() throws RippledClientErrorException {
+    Wallet sourceWallet = createRandomAccount();
+    Wallet destinationWallet = createRandomAccount();
+
+    FeeResult feeResult = xrplClient.fee();
+    AccountInfoResult accountInfoResult = this.scanForAccountInfo(sourceWallet.classicAddress());
+
+    Hash256 invoiceId = Hash256.of(Hashing.sha256().hashBytes("Check this out.".getBytes()).toString());
+    CheckCreate checkCreate = CheckCreate.builder()
+      .account(sourceWallet.classicAddress())
+      .fee(feeResult.drops().minimumFee())
+      .sequence(accountInfoResult.accountData().sequence())
+      .destination(destinationWallet.classicAddress())
+      .sendMax(XrpCurrencyAmount.of("12345"))
+      .invoiceId(invoiceId)
+      .signingPublicKey(sourceWallet.publicKey())
+      .build();
+
+    SubmissionResult<CheckCreate> response = xrplClient.submit(sourceWallet, checkCreate, CheckCreate.class);
+    assertThat(response.engineResult()).isNotEmpty().get().isEqualTo("tesSUCCESS");
+    logger.info(
+      "CheckCreate transaction successful: https://testnet.xrpl.org/transactions/{}",
+      response.transaction().hash().orElse("n/a")
+    );
+
+    Check check = (Check) scanAccountObjectsForCondition(sourceWallet.classicAddress(), result -> {
+      logger.info("AccountObjectsResult objects: {}", result.accountObjects());
+      return result.accountObjects().stream().anyMatch(findCheck(sourceWallet, destinationWallet, invoiceId));
+    })
+      .accountObjects().stream()
+      .filter(findCheck(sourceWallet, destinationWallet, invoiceId))
+      .findFirst().get();
+
+    feeResult = xrplClient.fee();
+    CheckCancel checkCancel = CheckCancel.builder()
+      .account(sourceWallet.classicAddress())
+      .sequence(accountInfoResult.accountData().sequence().plus(UnsignedInteger.ONE))
+      .fee(feeResult.drops().minimumFee())
+      .checkId(check.index())
+      .signingPublicKey(sourceWallet.publicKey())
+      .build();
+
+    SubmissionResult<CheckCancel> cancelResult = xrplClient.submit(sourceWallet, checkCancel, CheckCancel.class);
+    assertThat(cancelResult.engineResult()).isNotEmpty().get().isEqualTo("tesSUCCESS");
+    logger.info(
+      "CheckCancel transaction successful: https://testnet.xrpl.org/transactions/{}",
+      cancelResult.transaction().hash().orElse("n/a")
+    );
+
+    AccountObjectsResult accountObjectsResult = scanForAccountObjects(sourceWallet.classicAddress());
+    assertThat(accountObjectsResult.accountObjects()).matches(objects ->
+      objects.stream().noneMatch(findCheck(sourceWallet, destinationWallet, invoiceId))
+    );
+  }
+
+  @Test
+  public void createCheckAndDestinationCancels() throws RippledClientErrorException {
+    Wallet sourceWallet = createRandomAccount();
+    Wallet destinationWallet = createRandomAccount();
+
+    FeeResult feeResult = xrplClient.fee();
+    AccountInfoResult accountInfoResult = this.scanForAccountInfo(sourceWallet.classicAddress());
+
+    Hash256 invoiceId = Hash256.of(Hashing.sha256().hashBytes("Check this out.".getBytes()).toString());
+    CheckCreate checkCreate = CheckCreate.builder()
+      .account(sourceWallet.classicAddress())
+      .fee(feeResult.drops().minimumFee())
+      .sequence(accountInfoResult.accountData().sequence())
+      .destination(destinationWallet.classicAddress())
+      .sendMax(XrpCurrencyAmount.of("12345"))
+      .invoiceId(invoiceId)
+      .signingPublicKey(sourceWallet.publicKey())
+      .build();
+
+    SubmissionResult<CheckCreate> response = xrplClient.submit(sourceWallet, checkCreate, CheckCreate.class);
+    assertThat(response.engineResult()).isNotEmpty().get().isEqualTo("tesSUCCESS");
+    logger.info(
+      "CheckCreate transaction successful: https://testnet.xrpl.org/transactions/{}",
+      response.transaction().hash().orElse("n/a")
+    );
+
+    Check check = (Check) scanAccountObjectsForCondition(sourceWallet.classicAddress(), result -> {
+      logger.info("AccountObjectsResult objects: {}", result.accountObjects());
+      return result.accountObjects().stream().anyMatch(findCheck(sourceWallet, destinationWallet, invoiceId));
+    })
+      .accountObjects().stream()
+      .filter(findCheck(sourceWallet, destinationWallet, invoiceId))
+      .findFirst().get();
+
+    feeResult = xrplClient.fee();
+    AccountInfoResult destinationAccountInfo = this.scanForAccountInfo(destinationWallet.classicAddress());
+    CheckCancel checkCancel = CheckCancel.builder()
+      .account(destinationWallet.classicAddress())
+      .sequence(destinationAccountInfo.accountData().sequence())
+      .fee(feeResult.drops().minimumFee())
+      .checkId(check.index())
+      .signingPublicKey(destinationWallet.publicKey())
+      .build();
+
+    SubmissionResult<CheckCancel> cancelResult = xrplClient.submit(destinationWallet, checkCancel, CheckCancel.class);
+    assertThat(cancelResult.engineResult()).isNotEmpty().get().isEqualTo("tesSUCCESS");
+    logger.info(
+      "CheckCancel transaction successful: https://testnet.xrpl.org/transactions/{}",
+      cancelResult.transaction().hash().orElse("n/a")
+    );
+
+    AccountObjectsResult accountObjectsResult = scanForAccountObjects(sourceWallet.classicAddress());
+    assertThat(accountObjectsResult.accountObjects()).matches(objects ->
+      objects.stream().noneMatch(findCheck(sourceWallet, destinationWallet, invoiceId))
+    );
   }
 
   public Predicate<LedgerObject> findCheck(Wallet sourceWallet, Wallet destinationWallet, Hash256 invoiceId) {

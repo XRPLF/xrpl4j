0f67d58dcef6ac6aa6e9f4a48346f7be89a2eb19
nkramer44 <noah.ph.kramer@gmail.com>
2020-12-10 14:24:52 -0500

    AccountInfo json tests

--- xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/client/accounts/AccountInfoRequestParams.java
+++ xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/client/accounts/AccountInfoRequestParams.java
@@ -7,6 +7,7 @@ import org.immutables.value.Value;
 import org.xrpl.xrpl4j.model.client.common.LedgerIndex;
 import org.xrpl.xrpl4j.model.client.rippled.XrplRequestParams;
 import org.xrpl.xrpl4j.model.transactions.Address;
+import org.xrpl.xrpl4j.model.transactions.Hash256;
 
 import java.util.Optional;
 
@@ -28,7 +29,7 @@ public interface AccountInfoRequestParams extends XrplRequestParams {
   Address account();
 
   @JsonProperty("ledger_hash")
-  Optional<String> ledgerHash();
+  Optional<Hash256> ledgerHash();
 
   @JsonProperty("ledger_index")
   @Value.Default
@@ -49,7 +50,7 @@ public interface AccountInfoRequestParams extends XrplRequestParams {
   @Value.Default
   @JsonProperty("signer_lists")
   default boolean signerLists() {
-    return true;
+    return false;
   }
 
 }

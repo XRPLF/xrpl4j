753085d436394945a90b7fd0ff89b64d59083407
nkramer44 <noah.ph.kramer@gmail.com>
2021-02-10 14:30:03 -0500

    javadoc

--- xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/client/accounts/AccountTransactionsTransactionResult.java
+++ xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/client/accounts/AccountTransactionsTransactionResult.java
@@ -7,6 +7,11 @@ import org.immutables.value.Value;
 import org.xrpl.xrpl4j.model.client.XrplResult;
 import org.xrpl.xrpl4j.model.transactions.Transaction;
 
+/**
+ * The transaction that gets returned as part of a response to the account_tx rippled method.
+ *
+ * @param <T> The type of {@link Transaction}.
+ */
 @Value.Immutable
 @JsonSerialize(as = ImmutableAccountTransactionsTransactionResult.class)
 @JsonDeserialize(as = ImmutableAccountTransactionsTransactionResult.class)
@@ -16,9 +21,19 @@ public interface AccountTransactionsTransactionResult<T extends Transaction> ext
     return ImmutableAccountTransactionsTransactionResult.builder();
   }
 
+  /**
+   * The {@link Transaction}.
+   *
+   * @return A {@link T} with the transaction fields.
+   */
   @JsonProperty("tx")
   T transaction();
 
+  /**
+   * Whether or not this transaction came from a validated ledger.
+   *
+   * @return {@code true} if from a validated ledger, otherwise {@code false}.
+   */
   @Value.Default
   default boolean validated() {
     return false;

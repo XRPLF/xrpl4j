a87d521318e91ede15cf6f858cc8be31e89b6f59
nkramer44 <noah.ph.kramer@gmail.com>
2021-08-03 13:42:29 -0400

    unbreak others

--- xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/client/path/RipplePathFindRequestParams.java
+++ xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/client/path/RipplePathFindRequestParams.java
@@ -1,18 +1,23 @@
 package org.xrpl.xrpl4j.model.client.path;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.immutables.value.Value;
+import org.xrpl.xrpl4j.model.client.LegacyLedgerSpecifierUtils;
 import org.xrpl.xrpl4j.model.client.XrplRequestParams;
+import org.xrpl.xrpl4j.model.client.common.LedgerIndex;
 import org.xrpl.xrpl4j.model.client.common.LedgerIndexShortcut;
 import org.xrpl.xrpl4j.model.client.common.LedgerSpecifier;
 import org.xrpl.xrpl4j.model.transactions.Address;
 import org.xrpl.xrpl4j.model.transactions.CurrencyAmount;
+import org.xrpl.xrpl4j.model.transactions.Hash256;
 
 import java.util.List;
 import java.util.Optional;
+import javax.annotation.Nullable;
 
 /**
  * Request parameters for a "ripple_path_find" rippled API method call.
@@ -83,6 +88,29 @@ public interface RipplePathFindRequestParams extends XrplRequestParams {
   List<PathCurrency> sourceCurrencies();
 
   /**
+   * A 20-byte hex string for the ledger version to use.
+   *
+   * @return An optionally-present {@link Hash256}.
+   * @deprecated Ledger hash should be specified in {@link #ledgerSpecifier()}.
+   */
+  @JsonIgnore
+  @Deprecated
+  @Value.Auxiliary
+  Optional<Hash256> ledgerHash();
+
+  /**
+   * The ledger index of the ledger to use, or a shortcut string to choose a ledger automatically.
+   *
+   * @return A {@link LedgerIndex}.  Defaults to {@link LedgerIndex#CURRENT}.
+   * @deprecated Ledger index and any shortcut values should be specified in {@link #ledgerSpecifier()}.
+   */
+  @JsonIgnore
+  @Deprecated
+  @Nullable
+  @Value.Auxiliary
+  LedgerIndex ledgerIndex();
+
+  /**
    * Specifies the ledger version to request. A ledger version can be specified by ledger hash,
    * numerical ledger index, or a shortcut value.
    *
@@ -91,7 +119,6 @@ public interface RipplePathFindRequestParams extends XrplRequestParams {
   @Value.Default
   @JsonUnwrapped
   default LedgerSpecifier ledgerSpecifier() {
-    return LedgerSpecifier.ledgerIndexShortcut(LedgerIndexShortcut.CURRENT);
+    return LegacyLedgerSpecifierUtils.computeLedgerSpecifier(ledgerHash(), ledgerIndex());
   }
-
 }

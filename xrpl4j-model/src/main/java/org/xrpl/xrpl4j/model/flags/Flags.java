24355fdc17d4f40ee9ac6e48eb219315e4b2cf37
Mukul Jangid <49061120+mukulljangid@users.noreply.github.com>
2021-11-11 13:07:38 -0500

    add NFT flags to flags.Flags

--- xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/flags/Flags.java
+++ xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/flags/Flags.java
@@ -1294,4 +1294,239 @@ public class Flags {
       }
     }
   }
+
+  /**
+   * A set of static {@link TransactionFlags} which can be set on
+   * {@link org.xrpl.xrpl4j.model.transactions.NfTokenMint} transactions.
+   */
+  @SuppressWarnings("abbreviationaswordinname")
+  public static class NfTokenMintFlags extends TransactionFlags {
+
+    /**
+     * Constant {@link NfTokenMintFlags} for the {@code tfBurnable} flag.
+     */
+    protected static final NfTokenMintFlags BURNABLE = new NfTokenMintFlags(0x00000001);
+
+    /**
+     * Constant {@link NfTokenMintFlags} for the {@code tfOnlyXRP} flag.
+     */
+    protected static final NfTokenMintFlags ONLY_XRP = new NfTokenMintFlags(0x00000002);
+
+    /**
+     * Constant {@link NfTokenMintFlags} for the {@code tfTrustLine} flag.
+     */
+    protected static final NfTokenMintFlags TRUSTLINE = new NfTokenMintFlags(0x00000004);
+
+    /**
+     * Constant {@link NfTokenMintFlags} for the {@code tfTransferable} flag.
+     */
+    protected static final NfTokenMintFlags TRANSFERABLE = new NfTokenMintFlags(0x00000008);
+
+    private NfTokenMintFlags(long value) {
+      super(value);
+    }
+
+    /**
+     * Create a new {@link NfTokenMintFlags.Builder}.
+     *
+     * @return A new {@link NfTokenMintFlags.Builder}.
+     */
+    public static NfTokenMintFlags.Builder builder() {
+      return new Builder();
+    }
+
+    private static NfTokenMintFlags of(boolean tfFullyCanonicalSig, boolean tfBurnable, boolean tfOnlyXRP,
+                                       boolean tfTrustLine, boolean tfTransferable) {
+      return new NfTokenMintFlags(
+          TransactionFlags.of(
+              tfFullyCanonicalSig ? TransactionFlags.FULLY_CANONICAL_SIG : UNSET,
+              tfBurnable ? BURNABLE : UNSET,
+              tfOnlyXRP ? ONLY_XRP : UNSET,
+              tfTrustLine ? TRUSTLINE : UNSET,
+              tfTransferable ? TRANSFERABLE : UNSET
+          ).getValue()
+      );
+    }
+
+    /**
+     * Construct {@link NfTokenMintFlags} with a given value.
+     *
+     * @param value The long-number encoded flags value of this {@link NfTokenMintFlags}.
+     *
+     * @return New {@link NfTokenMintFlags}.
+     */
+    public static NfTokenMintFlags of(long value) {
+      return new NfTokenMintFlags(value);
+    }
+
+    /**
+     * Require a fully canonical transaction signature.
+     *
+     * @return {@code true} if {@code tfFullyCanonicalSig} is set, otherwise {@code false}.
+     */
+    public boolean tfFullyCanonicalSig() {
+      return this.isSet(TransactionFlags.FULLY_CANONICAL_SIG);
+    }
+
+    /**
+     * If set, indicates that the minted token may be burned by the issuer even
+     * if the issuer does not currently hold the token. The current holder of
+     * the token may always burn it.
+     *
+     * @return {@code true} if {@code tfBurnable} is set, otherwise {@code false}.
+     */
+    public boolean tfBurnable() {
+      return this.isSet(BURNABLE);
+    }
+
+    /**
+     * If set, indicates that the token may only be offered or sold for XRP.
+     *
+     * @return {@code true} if {@code tfOnlyXRP} is set, otherwise {@code false}.
+     */
+    public boolean tfOnlyXRP() {
+      return this.isSet(ONLY_XRP);
+    }
+
+    /**
+     * If set, indicates that the issuer wants a trustline to be automatically created.
+     *
+     * @return {@code true} if {@code tfTrustLine} is set, otherwise {@code false}.
+     */
+    public boolean tfTrustLine() {
+      return this.isSet(TRUSTLINE);
+    }
+
+    /**
+     * If set, indicates that this NfT can be transferred. This flag has no
+     * effect if the token is being transferred from the issuer or to the
+     * issuer.
+     *
+     * @return {@code true} if {@code tfTransferable} is set, otherwise {@code false}.
+     */
+    public boolean tfTransferable() {
+      return this.isSet(TRANSFERABLE);
+    }
+
+    /**
+     * A builder class for {@link NfTokenMintFlags}.
+     */
+    public static class Builder {
+      boolean tfFullyCanonicalSig = true;
+      boolean tfBurnable = false;
+      boolean tfOnlyXRP = false;
+      boolean tfTrustLine = false;
+      boolean tfTransferable = false;
+
+      /**
+       * Set {@code tfFullyCanonicalSig} to the given value.
+       *
+       * @param tfFullyCanonicalSig A boolean value.
+       *
+       * @return The same {@link NfTokenMintFlags.Builder}.
+       */
+      public NfTokenMintFlags.Builder tfFullyCanonicalSig(boolean tfFullyCanonicalSig) {
+        this.tfFullyCanonicalSig = tfFullyCanonicalSig;
+        return this;
+      }
+
+      /**
+       * Set {@code tfBurnable} to the given value.
+       *
+       * @param tfBurnable A boolean value.
+       *
+       * @return The same {@link NfTokenMintFlags.Builder}.
+       */
+      public NfTokenMintFlags.Builder tfBurnable(boolean tfBurnable) {
+        this.tfBurnable = tfBurnable;
+        return this;
+      }
+
+      /**
+       * Set {@code tfOnlyXRP} to the given value.
+       *
+       * @param tfOnlyXRP A boolean value.
+       *
+       * @return The same {@link NfTokenMintFlags.Builder}.
+       */
+      public NfTokenMintFlags.Builder tfOnlyXRP(boolean tfOnlyXRP) {
+        this.tfOnlyXRP = tfOnlyXRP;
+        return this;
+      }
+
+      /**
+       * Set {@code tfTrustLine} to the given value.
+       *
+       * @param tfTrustLine A boolean value.
+       *
+       * @return The same {@link NfTokenMintFlags.Builder}.
+       */
+      public NfTokenMintFlags.Builder tfTrustLine(boolean tfTrustLine) {
+        this.tfTrustLine = tfTrustLine;
+        return this;
+      }
+
+      /**
+       * Set {@code tfTransferable} to the given value.
+       *
+       * @param tfTransferable A boolean value.
+       *
+       * @return The same {@link NfTokenMintFlags.Builder}.
+       */
+      public NfTokenMintFlags.Builder tfTransferable(boolean tfTransferable) {
+        this.tfTransferable = tfTransferable;
+        return this;
+      }
+
+      /**
+       * Build a new {@link NfTokenMintFlags} from the current boolean values.
+       *
+       * @return A new {@link NfTokenMintFlags}.
+       */
+      public NfTokenMintFlags build() {
+        return NfTokenMintFlags.of(tfFullyCanonicalSig, tfBurnable, tfOnlyXRP, tfTrustLine, tfTransferable);
+      }
+    }
+  }
+
+  /**
+   * A set of static {@link Flags} which can be set on {@link org.xrpl.xrpl4j.model.transactions.NfTokenCreateOffer}s.
+   */
+  public static class NfTokenCreateOfferFlags extends Flags {
+
+    /**
+     * Constant for an unset flag.
+     */
+    //    public static final NfTokenCreateOfferFlags UNSET = new NfTokenCreateOfferFlags(0);
+
+    /**
+     * Constant {@link NfTokenCreateOfferFlags} for the {@code lsfOneOwner} flag.
+     */
+    public static final NfTokenCreateOfferFlags SELL_TOKEN = new NfTokenCreateOfferFlags(0x00000001);
+
+    private NfTokenCreateOfferFlags(long value) {
+      super(value);
+    }
+
+    /**
+     * Construct {@link NfTokenCreateOfferFlags} with a given value.
+     *
+     * @param value The long-number encoded flags value of this {@link NfTokenCreateOfferFlags}.
+     *
+     * @return New {@link NfTokenCreateOfferFlags}.
+     */
+    public static NfTokenCreateOfferFlags of(long value) {
+      return new NfTokenCreateOfferFlags(value);
+    }
+
+    /**
+     * If set, indicates that the offer is a sell offer.
+     * Otherwise, it is a buy offer.
+     *
+     *  @return {@code true} if {@code tfSellToken} is set, otherwise {@code false}.
+     */
+    public boolean tfSellToken() {
+      return this.isSet(NfTokenCreateOfferFlags.SELL_TOKEN);
+    }
+  }
 }

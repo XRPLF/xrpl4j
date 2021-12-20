b98ef706e37e582dbfc71ef3393a552fa4ee8b14
David Fuelling <323659+sappenin@users.noreply.github.com>
2021-12-20 15:45:37 -0700

    Fixes #211 Improve the way memos are constructed (#212)
    * Fixes #211 (Improve the way memos are constructed)
    Signed-off-by: David Fuelling <sappenin@gmail.com>

---
+++ xrpl4j-model/src/test/java/org/xrpl/xrpl4j/model/transactions/MemoTest.java
@@ -0,0 +1,113 @@
+package org.xrpl.xrpl4j.model.transactions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for {@link Memo}.
+ */
+@SuppressWarnings( {"OptionalGetWithoutIsPresent", "ResultOfMethodCallIgnored"})
+class MemoTest {
+
+  @Test
+  void testEmptyMemo() {
+    final Memo emptyMemo = Memo.builder().build();
+    assertThat(emptyMemo.memoData()).isEmpty();
+    assertThat(emptyMemo.memoType()).isEmpty();
+    assertThat(emptyMemo.memoFormat()).isEmpty();
+  }
+
+  @Test
+  void testSingleHexCharMemo() {
+    final Memo emptyMemo = Memo.builder()
+      .memoData("A")
+      .build();
+    assertThat(emptyMemo.memoData()).isNotEmpty();
+    assertThat(emptyMemo.memoData().get()).isEqualTo("A");
+    assertThat(emptyMemo.memoType()).isEmpty();
+    assertThat(emptyMemo.memoFormat()).isEmpty();
+  }
+
+  @Test
+  void lowercaseHexChars() {
+    final Memo emptyMemo = Memo.builder()
+      .memoData("abcdef1234")
+      .memoType("abcdef1234")
+      .memoFormat("abcdef1234")
+      .build();
+    assertThat(emptyMemo.memoData()).isNotEmpty();
+    assertThat(emptyMemo.memoData().get()).isEqualTo("abcdef1234");
+    assertThat(emptyMemo.memoType()).isNotEmpty();
+    assertThat(emptyMemo.memoType().get()).isEqualTo("abcdef1234");
+    assertThat(emptyMemo.memoFormat()).isNotEmpty();
+    assertThat(emptyMemo.memoFormat().get()).isEqualTo("abcdef1234");
+  }
+
+  @Test
+  void uppercaseHexChars() {
+    final Memo emptyMemo = Memo.builder()
+      .memoData("ABCDEF1234")
+      .memoType("ABCDEF1234")
+      .memoFormat("ABCDEF1234")
+      .build();
+    assertThat(emptyMemo.memoData()).isNotEmpty();
+    assertThat(emptyMemo.memoData().get()).isEqualTo("ABCDEF1234");
+    assertThat(emptyMemo.memoType()).isNotEmpty();
+    assertThat(emptyMemo.memoType().get()).isEqualTo("ABCDEF1234");
+    assertThat(emptyMemo.memoFormat()).isNotEmpty();
+    assertThat(emptyMemo.memoFormat().get()).isEqualTo("ABCDEF1234");
+  }
+
+  @Test
+  void testWithPlaintextSingleChar() {
+    Memo memo = Memo.withPlaintext("Z").build();
+    assertThat(memo.memoData()).isNotEmpty();
+    assertThat(memo.memoData().get()).isEqualTo("5A");
+    assertThat(memo.memoType()).isEmpty();
+    assertThat(memo.memoFormat()).isNotEmpty();
+    assertThat(memo.memoFormat().get()).isEqualTo("746578742F706C61696E");
+  }
+
+  @Test
+  void testMemoWithNonHexFields() {
+    IllegalStateException thrownException = Assertions.assertThrows(IllegalStateException.class, () -> Memo.builder()
+      .memoData("Hello World")
+      .memoType("0123456789ABCDEF")
+      .memoFormat("0123456789ABCDEF")
+      .build());
+    assertThat(thrownException.getMessage()).isEqualTo("MemoData must be a hex-encoded string");
+
+    thrownException = Assertions.assertThrows(IllegalStateException.class, () -> Memo.builder()
+      .memoData("0123456789ABCDEF")
+      .memoType("Hello World")
+      .memoFormat("0123456789ABCDEF")
+      .build());
+    assertThat(thrownException.getMessage()).isEqualTo("MemoType must be a hex-encoded string");
+
+    thrownException = Assertions.assertThrows(IllegalStateException.class, () -> Memo.builder()
+      .memoData("0123456789ABCDEF")
+      .memoType("0123456789ABCDEF")
+      .memoFormat("Hello World")
+      .build());
+    assertThat(thrownException.getMessage()).isEqualTo("MemoFormat must be a hex-encoded string");
+  }
+
+  @Test
+  void testMemoWithAllHexFields() {
+    assertThat(Memo.builder()
+      .memoData("0123456789ABCDEF")
+      .memoType("0123456789ABCDEF")
+      .memoFormat("0123456789ABCDEF")
+      .build()).isNotNull();
+  }
+
+  @Test
+  void testBuildFromPlain() {
+    final Memo plainTextMemo = Memo.withPlaintext("Hello World").build();
+    assertThat(plainTextMemo.memoType()).isEmpty();
+    assertThat(plainTextMemo.memoFormat().get()).isEqualTo("746578742F706C61696E");
+    assertThat(plainTextMemo.memoData().get()).isEqualTo("48656C6C6F20576F726C64");
+  }
+}

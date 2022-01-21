43b2731926de0254726d86b6a9ff09ad2b5caa2c
mukulljangid <49061120+mukulljangid@users.noreply.github.com>
2022-01-21 12:41:06 -0500

    add tests for Nft buy and sell requests

---
+++ xrpl4j-model/src/test/java/org/xrpl/xrpl4j/model/client/nft/NftBuyOffersResultTest.java
@@ -0,0 +1,36 @@
+package org.xrpl.xrpl4j.model.client.nft;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.json.JSONException;
+import org.junit.jupiter.api.Test;
+import org.xrpl.xrpl4j.model.AbstractJsonTest;
+import org.xrpl.xrpl4j.model.transactions.XrpCurrencyAmount;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NftBuyOffersResultTest extends AbstractJsonTest {
+
+  @Test
+  public void test() throws JsonProcessingException, JSONException {
+
+    NfTokenOfferObject obj = NfTokenOfferObject.builder()
+      .amount(XrpCurrencyAmount.ofDrops(100000))
+      .build();
+
+    List<NfTokenOfferObject> list = new ArrayList<>();
+    list.add(obj);
+
+    NftBuyOffersResult params = NftBuyOffersResult.builder()
+      .tokenId("000100001E962F495F07A990F4ED55ACCFEEF365DBAA76B6A048C0A200000007")
+      .offers(list)
+      .build();
+
+    String json = "{\n" +
+      "        \"TokenID\": \"000100001E962F495F07A990F4ED55ACCFEEF365DBAA76B6A048C0A200000007\",\n" +
+      "        \"offers\": [{\"Amount\":\"100000\"}]" +
+      "    }";
+
+    assertCanSerializeAndDeserialize(params, json);
+  }
+}

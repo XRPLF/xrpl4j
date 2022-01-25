30f4a02dadcc0057a2234ffde9e0f4eb4ebac8c9
mukulljangid <49061120+mukulljangid@users.noreply.github.com>
2022-01-25 16:39:07 -0500

    addd NFTokenOffer object LedgerObject

--- xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/ledger/LedgerObject.java
+++ xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/ledger/LedgerObject.java
@@ -86,7 +86,7 @@ public interface LedgerObject {
     /**
      * The {@link LedgerEntryType} for {@code NFTokenOffer} ledger objects.
      */
-    NFTOKEN_OFFER("NfTokenOffer"),
+    NFTOKEN_OFFER("NFTokenOffer"),
 
     /**
      * The {@link LedgerEntryType} for {@code Offer} ledger objects.

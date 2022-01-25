bbe4feb3fff10313cbeb2e97d17cd12360424dcf
mukulljangid <49061120+mukulljangid@users.noreply.github.com>
2022-01-25 16:22:08 -0500

    create class TransferFee and it value checks
    also add its serialize and deserialize classes

--- xrpl4j-model/src/test/java/org/xrpl/xrpl4j/model/transactions/NfTokenMintTest.java
+++ xrpl4j-model/src/test/java/org/xrpl/xrpl4j/model/transactions/NfTokenMintTest.java
@@ -3,8 +3,13 @@ package org.xrpl.xrpl4j.model.transactions;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.google.common.primitives.UnsignedInteger;
 import com.google.common.primitives.UnsignedLong;
 import org.junit.jupiter.api.Test;
+import org.xrpl.xrpl4j.model.flags.Flags;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 public class NfTokenMintTest {
 
@@ -33,4 +38,67 @@ public class NfTokenMintTest {
         .build()
     );
   }
+
+  @Test
+  public void buildTxWithTransferFee() {
+    TransferFee transferFee = TransferFee.of(UnsignedInteger.valueOf(10));
+    NfTokenMint nfTokenMint = NfTokenMint.builder()
+      .fee(XrpCurrencyAmount.ofDrops(1))
+      .account(Address.of("rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59Ba"))
+      .tokenTaxon(UnsignedLong.valueOf(146999694L))
+      .flags(Flags.NfTokenMintFlags.builder()
+        .tfTransferable(true)
+        .build())
+      .transferFee(transferFee)
+      .build();
+
+    assertThat(nfTokenMint.transferFee()).isEqualTo(Optional.of(transferFee));
+  }
+
+  @Test
+  public void transferFeeWithoutFlagSet() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> NfTokenMint.builder()
+        .fee(XrpCurrencyAmount.ofDrops(1))
+        .account(Address.of("rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59Ba"))
+        .tokenTaxon(UnsignedLong.valueOf(146999694L))
+        .transferFee(TransferFee.of(UnsignedInteger.valueOf(1000)))
+        .build(),
+      "tfTransferable flag must be set for secondary sale."
+    );
+  }
+
+  @Test
+  public void transferFeeOutOfBounds() {
+    assertThrows(
+      IllegalArgumentException.class,
+      () -> NfTokenMint.builder()
+        .fee(XrpCurrencyAmount.ofDrops(1))
+        .account(Address.of("rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59Ba"))
+        .tokenTaxon(UnsignedLong.valueOf(146999694L))
+        .flags(Flags.NfTokenMintFlags.builder()
+          .tfTransferable(true)
+          .build())
+        .transferFee(TransferFee.of(UnsignedInteger.valueOf(10000)))
+        .build(),
+      "TransferFee should be in the range 0 to 9999."
+    );
+  }
+
+  @Test
+  public void transferFeeUsingPercent() {
+    NfTokenMint nfTokenMint = NfTokenMint.builder()
+      .fee(XrpCurrencyAmount.ofDrops(1))
+      .account(Address.of("rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59Ba"))
+      .tokenTaxon(UnsignedLong.valueOf(146999694L))
+      .flags(Flags.NfTokenMintFlags.builder()
+        .tfTransferable(true)
+        .build())
+      .transferFee(TransferFee.ofPercent(BigDecimal.valueOf(99.99)))
+      .build();
+
+    assertThat(nfTokenMint.transferFee().equals(Optional.of(9999)));
+  }
+
 }

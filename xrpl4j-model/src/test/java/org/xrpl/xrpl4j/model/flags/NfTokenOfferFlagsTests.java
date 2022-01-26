e4a4bf5c8c7833767779364faa8f988d1c3d5266
mukulljangid <49061120+mukulljangid@users.noreply.github.com>
2022-01-26 05:50:12 -0500

    Add flags and missing fields to `NfTokenOfferObject`

---
+++ xrpl4j-model/src/test/java/org/xrpl/xrpl4j/model/flags/NfTokenOfferFlagsTests.java
@@ -0,0 +1,32 @@
+package org.xrpl.xrpl4j.model.flags;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class NfTokenOfferFlagsTests extends AbstractFlagsTest {
+
+  public static Stream<Arguments> data() {
+    return getBooleanCombinations(2);
+  }
+
+  @ParameterizedTest
+  @MethodSource("data")
+  public void testDeriveIndividualFlagsFromFlags(
+    boolean lsfBuyToken,
+    boolean lsfAuthorized
+  ) {
+    long expectedFlags = (lsfBuyToken ? Flags.NfTokenOfferFlags.BUY_TOKEN.getValue() : 0L) |
+      (lsfAuthorized ? Flags.NfTokenOfferFlags.AUTHORIZED.getValue() : 0L);
+
+    Flags.NfTokenOfferFlags flags = Flags.NfTokenOfferFlags.of(expectedFlags);
+
+    assertThat(flags.getValue()).isEqualTo(expectedFlags);
+    assertThat(flags.lsfBuyToken()).isEqualTo(lsfBuyToken);
+    assertThat(flags.lsfAuthorized()).isEqualTo(lsfAuthorized);
+  }
+}

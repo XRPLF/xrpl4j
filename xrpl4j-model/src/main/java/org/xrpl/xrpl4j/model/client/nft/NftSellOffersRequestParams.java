308113f8c70bc6ee61d87a2b277e5fca6327032a
mukulljangid <49061120+mukulljangid@users.noreply.github.com>
2022-01-26 13:48:35 -0500

    add limit and marker to buy and sell offers request

--- xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/client/nft/NftSellOffersRequestParams.java
+++ xrpl4j-model/src/main/java/org/xrpl/xrpl4j/model/client/nft/NftSellOffersRequestParams.java
@@ -3,9 +3,13 @@ package org.xrpl.xrpl4j.model.client.nft;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.google.common.primitives.UnsignedInteger;
 import org.immutables.value.Value;
 import org.xrpl.xrpl4j.model.client.XrplRequestParams;
+import org.xrpl.xrpl4j.model.transactions.Marker;
 import org.xrpl.xrpl4j.model.transactions.NfTokenId;
+
+import java.util.Optional;
 
 /**
  * Request params for "nft_sell_offers" rippled API method call.
@@ -31,4 +35,19 @@ public interface NftSellOffersRequestParams extends XrplRequestParams {
    */
   @JsonProperty("TokenID")
   NfTokenId tokenId();
+
+  /**
+   * Limit the number of sell offers for the {@link NfTokenId}. The server is not required to honor
+   * this value. Must be within the inclusive range 10 to 400.
+   *
+   * @return An optionally-present {@link UnsignedInteger} representing the response limit.
+   */
+  Optional<UnsignedInteger> limit();
+
+  /**
+   * Value from a previous paginated response. Resume retrieving data where that response left off.
+   *
+   * @return An optionally-present {@link String} containing the marker.
+   */
+  Optional<Marker> marker();
 }

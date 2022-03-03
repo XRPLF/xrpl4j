0849dce00e5425d4ab5bd8c7c4748909cef18365
mukulljangid <49061120+mukulljangid@users.noreply.github.com>
2022-03-03 17:40:46 -0500

    add sameness checks for all values

--- xrpl4j-model/src/test/java/org/xrpl/xrpl4j/model/immutables/FluentCompareToTest.java
+++ xrpl4j-model/src/test/java/org/xrpl/xrpl4j/model/immutables/FluentCompareToTest.java
@@ -1,6 +1,7 @@
 package org.xrpl.xrpl4j.model.immutables;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import com.google.common.primitives.UnsignedInteger;
 import org.junit.jupiter.api.Test;
@@ -24,20 +25,28 @@ public class FluentCompareToTest {
     assert (FluentCompareTo.is(SMALLER).lessThan(BIGGER));
     assert (FluentCompareTo.is(SMALLER).notGreaterThan(BIGGER));
     assert (FluentCompareTo.is(SMALLER).notGreaterThanEqualTo(BIGGER));
+    assertFalse(FluentCompareTo.is(SMALLER).greaterThan(SMALLER));
+    assertFalse(FluentCompareTo.is(SMALLER).lessThan(SMALLER));
 
     assert (FluentCompareTo.is(BIGGER).greaterThanEqualTo(BIGGER));
     assert (FluentCompareTo.is(BIGGER).greaterThanEqualTo(SMALLER));
     assert (FluentCompareTo.is(BIGGER).greaterThan(SMALLER));
     assert (FluentCompareTo.is(BIGGER).notLessThan(SMALLER));
     assert (FluentCompareTo.is(BIGGER).notLessThanOrEqualTo(SMALLER));
+    assertFalse(FluentCompareTo.is(BIGGER).greaterThan(BIGGER));
+    assertFalse(FluentCompareTo.is(BIGGER).lessThan(BIGGER));
 
     assert (FluentCompareTo.is(SMALLER).between(SMALLER, MAX_UINT));
-    assert (!FluentCompareTo.is(BIGGER).betweenExclusive(SMALLER, BIGGER));
+    assertFalse(FluentCompareTo.is(BIGGER).betweenExclusive(SMALLER, BIGGER));
     assert (FluentCompareTo.is(MAX_UINT).notBetween(SMALLER, BIGGER));
-    assert (!FluentCompareTo.is(BIGGER).notBetweenExclusive(SMALLER, MAX_UINT));
+    assertFalse(FluentCompareTo.is(BIGGER).notBetweenExclusive(SMALLER, MAX_UINT));
 
     assertThat(FluentCompareTo.is(SMALLER).getValue()).isEqualTo(UnsignedInteger.valueOf(1));
     assertThat(FluentCompareTo.is(BIGGER).getValue()).isEqualTo(UnsignedInteger.valueOf(100));
     assertThat(FluentCompareTo.is(SMALLER)).isNotEqualTo(FluentCompareTo.is(BIGGER));
+
+    assert (FluentCompareTo.is(MAX_UINT).equalTo(MAX_UINT));
+    assertFalse(FluentCompareTo.is(MAX_UINT).greaterThan(MAX_UINT));
+    assertFalse(FluentCompareTo.is(MAX_UINT).lessThan(MAX_UINT));
   }
 }

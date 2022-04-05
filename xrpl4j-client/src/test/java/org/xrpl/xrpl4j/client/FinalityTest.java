2da2f413498aa2d87fe91921aad0db94f7445280
mukulljangid <49061120+mukulljangid@users.noreply.github.com>
2022-04-05 15:45:02 -0400

    add tests for Finality interface

---
+++ xrpl4j-client/src/test/java/org/xrpl/xrpl4j/client/FinalityTest.java
@@ -0,0 +1,33 @@
+package org.xrpl.xrpl4j.client;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class FinalityTest {
+
+  @Test
+  public void createFinalityObjectTest() {
+
+    Finality finality = Finality.builder()
+      .finalityStatus(FinalityStatus.NOT_FINAL)
+      .resultCode("tesSUCCESS")
+      .build();
+
+    assertThat(finality.finalityStatus()).isEqualTo(FinalityStatus.NOT_FINAL);
+    assertThat(finality.resultCode().get()).isEqualTo("tesSUCCESS");
+    assertThat(finality.resultCodeSafe()).isEqualTo("tesSUCCESS");
+  }
+
+  @Test
+  public void throwForMissingResultCodeTest() {
+
+    Finality finality = Finality.builder()
+      .finalityStatus(FinalityStatus.NOT_FINAL)
+      .build();
+
+    assertThat(finality.finalityStatus()).isEqualTo(FinalityStatus.NOT_FINAL);
+    assertThrows(IllegalStateException.class, () -> finality.resultCodeSafe());
+  }
+}

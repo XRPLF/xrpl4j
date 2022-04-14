647bcd95268f6e81a7897ae852cf5da989543730
mukulljangid <49061120+mukulljangid@users.noreply.github.com>
2022-04-14 16:36:25 -0400

    use known testnet address and fetch transactions for it

--- xrpl4j-integration-tests/src/test/java/org/xrpl/xrpl4j/tests/AccountTransactionsIT.java
+++ xrpl4j-integration-tests/src/test/java/org/xrpl/xrpl4j/tests/AccountTransactionsIT.java
@@ -39,20 +39,20 @@ import org.xrpl.xrpl4j.model.client.ledger.LedgerRequestParams;
 import org.xrpl.xrpl4j.model.client.ledger.LedgerResult;
 import org.xrpl.xrpl4j.model.transactions.Address;
 import org.xrpl.xrpl4j.model.transactions.Hash256;
-import org.xrpl.xrpl4j.tests.environment.MainnetEnvironment;
+import org.xrpl.xrpl4j.tests.environment.TestnetEnvironment;
 
 import java.util.concurrent.TimeUnit;
 
 public class AccountTransactionsIT {
 
-  // an arbitrary address on xrpl mainnet that has a decent amount of transaction history
-  public static final Address MAINNET_ADDRESS = Address.of("r9m6MwViR4GnUNqoGXGa8eroBrZ9FAPHFS");
+  // an arbitrary address on xrpl testnet that has a decent amount of transaction history
+  public static final Address TESTNET_ADDRESS = Address.of("rURUD69hsgJYtrt1xJ4drhs45qRirg36CU");
 
-  private final XrplClient mainnetClient = new MainnetEnvironment().getXrplClient();
+  private final XrplClient testnetClient = new TestnetEnvironment().getXrplClient();
 
   @Test
   public void listTransactionsDefaultWithPagination() throws JsonRpcClientErrorException {
-    AccountTransactionsResult results = mainnetClient.accountTransactions(MAINNET_ADDRESS);
+    AccountTransactionsResult results = testnetClient.accountTransactions(TESTNET_ADDRESS);
 
     assertThat(results.transactions()).hasSize(200);
     assertThat(results.marker()).isNotEmpty();
@@ -61,13 +61,13 @@ public class AccountTransactionsIT {
   @Test
   @Timeout(30)
   public void listTransactionsPagination() throws JsonRpcClientErrorException {
-    final int expectedTransactions = 748;
-    // known ledger index range for this account that is known to have exactly 748 transactions
-    LedgerIndexBound minLedger = LedgerIndexBound.of(61400000);
-    LedgerIndexBound maxLedger = LedgerIndexBound.of(61487000);
-    AccountTransactionsResult results = mainnetClient.accountTransactions(
+    final int expectedTransactions = 421;
+    // known ledger index range for this account that is known to have exactly 421 transactions
+    LedgerIndexBound minLedger = LedgerIndexBound.of(26774141);
+    LedgerIndexBound maxLedger = LedgerIndexBound.of(31614070);
+    AccountTransactionsResult results = testnetClient.accountTransactions(
       AccountTransactionsRequestParams.builder(minLedger, maxLedger)
-        .account(MAINNET_ADDRESS)
+        .account(TESTNET_ADDRESS)
         .build()
     );
     assertThat(results.transactions()).isNotEmpty();
@@ -76,9 +76,9 @@ public class AccountTransactionsIT {
     int transactionsFound = results.transactions().size();
     int pages = 1;
     while (results.marker().isPresent()) {
-      results = mainnetClient.accountTransactions(AccountTransactionsRequestParams
+      results = testnetClient.accountTransactions(AccountTransactionsRequestParams
           .builder(minLedger, maxLedger)
-        .account(MAINNET_ADDRESS)
+        .account(TESTNET_ADDRESS)
         .marker(results.marker().get())
         .build());
       assertThat(results.transactions()).isNotEmpty();
@@ -87,47 +87,47 @@ public class AccountTransactionsIT {
     }
 
     assertThat(transactionsFound).isEqualTo(expectedTransactions);
-    assertThat(pages).isEqualTo(4);
+    assertThat(pages).isEqualTo(3);
   }
 
   @Test
   public void listTransactionWithIndexRange() {
     // also arbitrary indexes chosen because they have known transaction results we can check
-    LedgerIndexBound minLedger = LedgerIndexBound.of(61486000);
-    LedgerIndexBound maxLedger = LedgerIndexBound.of(61487000);
+    LedgerIndexBound minLedger = LedgerIndexBound.of(26774141);
+    LedgerIndexBound maxLedger = LedgerIndexBound.of(26936155);
 
     // Sometimes we will get a "server busy" error back in this test, so if we do get that, we should just wait
     // a few seconds until asking again.
     AccountTransactionsResult results = getAccountTransactions(
       AccountTransactionsRequestParams.builder(minLedger, maxLedger)
-        .account(MAINNET_ADDRESS)
+        .account(TESTNET_ADDRESS)
         .build()
     );
 
     assertThat(results.ledgerIndexMinimum()).isEqualTo(minLedger);
     assertThat(results.ledgerIndexMaximum()).isEqualTo(maxLedger);
-    assertThat(results.transactions()).hasSize(16);
+    assertThat(results.transactions()).hasSize(200);
     // results are returned in descending sorted order by ledger index
     assertThat(results.transactions().get(0).transaction().ledgerIndex()).isNotEmpty().get()
       .isEqualTo(results.transactions().get(0).resultTransaction().ledgerIndex());
     assertThat(results.transactions().get(0).resultTransaction().ledgerIndex())
-      .isEqualTo(LedgerIndex.of(UnsignedInteger.valueOf(61486994)));
+      .isEqualTo(LedgerIndex.of(UnsignedInteger.valueOf(26935924)));
 
     assertThat(results.transactions().get(15).transaction().ledgerIndex()).isNotEmpty().get()
       .isEqualTo(results.transactions().get(15).resultTransaction().ledgerIndex());
     assertThat(results.transactions().get(15).resultTransaction().ledgerIndex())
-      .isEqualTo(LedgerIndex.of(UnsignedInteger.valueOf(61486026)));
+      .isEqualTo(LedgerIndex.of(UnsignedInteger.valueOf(26935923)));
   }
 
   @Test
   void listTransactionsWithLedgerSpecifiers() throws JsonRpcClientErrorException {
     AccountTransactionsResult resultByShortcut = getAccountTransactions(
       AccountTransactionsRequestParams.builder(LedgerSpecifier.VALIDATED)
-        .account(MAINNET_ADDRESS)
+        .account(TESTNET_ADDRESS)
         .build()
     );
 
-    LedgerResult ledger = mainnetClient.ledger(
+    LedgerResult ledger = testnetClient.ledger(
       LedgerRequestParams.builder()
         .ledgerSpecifier(
           LedgerSpecifier.of(
@@ -144,13 +144,13 @@ public class AccountTransactionsIT {
 
     AccountTransactionsResult resultByLedgerIndex = getAccountTransactions(
       AccountTransactionsRequestParams.builder(LedgerSpecifier.of(validatedLedgerIndex))
-        .account(MAINNET_ADDRESS)
+        .account(TESTNET_ADDRESS)
         .build()
     );
 
     AccountTransactionsResult resultByLedgerHash = getAccountTransactions(
       AccountTransactionsRequestParams.builder(LedgerSpecifier.of(validatedLedgerHash))
-        .account(MAINNET_ADDRESS)
+        .account(TESTNET_ADDRESS)
         .build()
     );
 
@@ -168,7 +168,7 @@ public class AccountTransactionsIT {
       .await()
       .until(() -> {
         try {
-          return mainnetClient.accountTransactions(params);
+          return testnetClient.accountTransactions(params);
         } catch (JsonRpcClientErrorException e) {
           if (e.getMessage().equals("The server is too busy to help you now.")) {
             return null;

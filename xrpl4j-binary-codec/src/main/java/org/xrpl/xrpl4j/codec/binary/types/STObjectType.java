2d71760f2f8a8e7116ad13cf1b9b5434376f0515
mukulljangid <49061120+mukulljangid@users.noreply.github.com>
2022-04-19 17:22:58 -0400

    add IT(rev order) and unit test to add coverage

--- xrpl4j-binary-codec/src/main/java/org/xrpl/xrpl4j/codec/binary/types/STObjectType.java
+++ xrpl4j-binary-codec/src/main/java/org/xrpl/xrpl4j/codec/binary/types/STObjectType.java
@@ -9,9 +9,9 @@ package org.xrpl.xrpl4j.codec.binary.types;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -108,10 +108,10 @@ public class STObjectType extends SerializedType<STObjectType> {
       JsonNode fieldNode;
       // rippled expects signers canonically based on address
       if (fieldName.equals("Signers")) {
-        final AddressCodec addressCodec = new AddressCodec();
+        final AddressCodec addressCodec = AddressCodec.getInstance();
         ArrayNode arrayNode = (ArrayNode) node.get(fieldName);
         List<JsonNode> jsonNodeList = new ArrayList<>();
-        for (JsonNode x: arrayNode) {
+        for (JsonNode x : arrayNode) {
           jsonNodeList.add(x);
         }
         List<JsonNode> jsonNodesSorted = jsonNodeList.stream().sorted(
--- xrpl4j-integration-tests/src/test/java/org/xrpl/xrpl4j/tests/SubmitMultisignedIT.java
+++ xrpl4j-integration-tests/src/test/java/org/xrpl/xrpl4j/tests/SubmitMultisignedIT.java
@@ -6,8 +6,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.primitives.UnsignedInteger;
 import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.xrpl.xrpl4j.client.JsonRpcClientErrorException;
+import org.xrpl.xrpl4j.codec.addresses.AddressCodec;
 import org.xrpl.xrpl4j.codec.binary.XrplBinaryCodec;
 import org.xrpl.xrpl4j.keypairs.DefaultKeyPairService;
 import org.xrpl.xrpl4j.keypairs.KeyPairService;
@@ -19,6 +21,7 @@ import org.xrpl.xrpl4j.model.client.transactions.SubmitResult;
 import org.xrpl.xrpl4j.model.jackson.ObjectMapperFactory;
 import org.xrpl.xrpl4j.model.ledger.SignerEntry;
 import org.xrpl.xrpl4j.model.ledger.SignerEntryWrapper;
+import org.xrpl.xrpl4j.model.transactions.Address;
 import org.xrpl.xrpl4j.model.transactions.Payment;
 import org.xrpl.xrpl4j.model.transactions.Signer;
 import org.xrpl.xrpl4j.model.transactions.SignerListSet;
@@ -28,6 +31,7 @@ import org.xrpl.xrpl4j.model.transactions.TransactionResultCodes;
 import org.xrpl.xrpl4j.model.transactions.XrpCurrencyAmount;
 import org.xrpl.xrpl4j.wallet.Wallet;
 
+import java.math.BigInteger;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,15 +42,20 @@ public class SubmitMultisignedIT extends AbstractIT {
   protected final XrplBinaryCodec binaryCodec = new XrplBinaryCodec();
   protected final KeyPairService keyPairService = DefaultKeyPairService.getInstance();
 
-  @Test
-  public void submitMultisignedAndVerifyHash() throws JsonRpcClientErrorException, JsonProcessingException {
-    /////////////////////////////
-    // Create four accounts, one for the multisign account owner, one for their two friends,
-    // and one to send a Payment to.
-    Wallet sourceWallet = createRandomAccount();
-    Wallet aliceWallet = createRandomAccount();
-    Wallet bobWallet = createRandomAccount();
-    Wallet destinationWallet = createRandomAccount();
+  /////////////////////////////
+  // Create four accounts, one for the multisign account owner, one for their two friends,
+  // and one to send a Payment to.
+  Wallet sourceWallet = createRandomAccount();
+  Wallet aliceWallet = createRandomAccount();
+  Wallet bobWallet = createRandomAccount();
+  Wallet destinationWallet = createRandomAccount();
+
+  FeeResult feeResult;
+  AccountInfoResult sourceAccountInfoAfterSignerListSet;
+  SubmitResult<SignerListSet> signerListSetResult;
+
+  @BeforeEach
+  public void setUp() throws JsonRpcClientErrorException {
 
     /////////////////////////////
     // Wait for all of the accounts to show up in a validated ledger
@@ -63,7 +72,7 @@ public class SubmitMultisignedIT extends AbstractIT {
 
     /////////////////////////////
     // Then submit a SignerListSet transaction to add alice and bob as signers on the account
-    FeeResult feeResult = xrplClient.fee();
+    feeResult = xrplClient.fee();
     SignerListSet signerListSet = SignerListSet.builder()
       .account(sourceWallet.classicAddress())
       .fee(feeResult.drops().openLedgerFee())
@@ -88,7 +97,7 @@ public class SubmitMultisignedIT extends AbstractIT {
 
     /////////////////////////////
     // Validate that the transaction was submitted successfully
-    SubmitResult<SignerListSet> signerListSetResult = xrplClient.submit(sourceWallet, signerListSet);
+    signerListSetResult = xrplClient.submit(sourceWallet, signerListSet);
     assertThat(signerListSetResult.result()).isEqualTo(TransactionResultCodes.TES_SUCCESS);
     assertThat(signerListSetResult.transactionResult().transaction().hash()).isNotEmpty().get()
       .isEqualTo(signerListSetResult.transactionResult().hash());
@@ -100,7 +109,7 @@ public class SubmitMultisignedIT extends AbstractIT {
     /////////////////////////////
     // Then wait until the transaction enters a validated ledger and the source account's signer list
     // exists
-    AccountInfoResult sourceAccountInfoAfterSignerListSet = scanForResult(
+    sourceAccountInfoAfterSignerListSet = scanForResult(
       () -> this.getValidatedAccountInfo(sourceWallet.classicAddress()),
       infoResult -> infoResult.accountData().signerLists().size() == 1
     );
@@ -113,6 +122,10 @@ public class SubmitMultisignedIT extends AbstractIT {
     ).isEqualTo(signerListSet.signerEntries().stream()
       .sorted(Comparator.comparing(entry -> entry.signerEntry().account()))
       .collect(Collectors.toList()));
+  }
+
+  @Test
+  public void submitMultisignedAndVerifyHash() throws JsonRpcClientErrorException, JsonProcessingException {
 
     /////////////////////////////
     // Construct an unsigned Payment transaction to be multisigned
@@ -152,6 +165,85 @@ public class SubmitMultisignedIT extends AbstractIT {
         }
       )
       .collect(Collectors.toList());
+
+    /////////////////////////////
+    // Then we add the signatures to the Payment object and submit it
+    Payment multiSigPayment = Payment.builder()
+      .from(unsignedPayment)
+      .signers(signers)
+      .build();
+
+    SignedTransaction<Payment> signedTransaction = SignedTransaction.<Payment>builder()
+      .signedTransaction(multiSigPayment)
+      .signedTransactionBlob(binaryCodec.encode(objectMapper.writeValueAsString(multiSigPayment)))
+      .build();
+    String libraryCalculatedHash = signedTransaction.hash().value();
+
+    SubmitMultiSignedResult<Payment> paymentResult = xrplClient.submitMultisigned(multiSigPayment);
+
+    assertThat(paymentResult.transaction().hash().value()).isEqualTo(libraryCalculatedHash);
+
+    assertThat(paymentResult.result()).isEqualTo(TransactionResultCodes.TES_SUCCESS);
+    assertThat(signerListSetResult.transactionResult().transaction().hash()).isNotEmpty().get()
+      .isEqualTo(signerListSetResult.transactionResult().hash());
+    logger.info(
+      "Payment transaction successful: https://testnet.xrpl.org/transactions/" +
+        paymentResult.transaction().hash()
+    );
+  }
+
+  @Test
+  public void submitMultisignedWithSignersInDescOrderAndVerifyHash() throws
+    JsonRpcClientErrorException, JsonProcessingException {
+
+    /////////////////////////////
+    // Construct an unsigned Payment transaction to be multisigned
+    Payment unsignedPayment = Payment.builder()
+      .account(sourceWallet.classicAddress())
+      .fee(
+        Transaction.computeMultiSigFee(
+          feeResult.drops().openLedgerFee(),
+          sourceAccountInfoAfterSignerListSet.accountData().signerLists().get(0)
+        )
+      )
+      .sequence(sourceAccountInfoAfterSignerListSet.accountData().sequence())
+      .amount(XrpCurrencyAmount.ofDrops(12345))
+      .destination(destinationWallet.classicAddress())
+      .signingPublicKey("")
+      .build();
+
+    /////////////////////////////
+    // Alice and Bob sign the transaction with their private keys
+    List<SignerWrapper> signers = Lists.newArrayList(aliceWallet, bobWallet).stream()
+      .map(wallet -> {
+          try {
+            String unsignedJson = objectMapper.writeValueAsString(unsignedPayment);
+
+            String unsignedBinaryHex = binaryCodec.encodeForMultiSigning(unsignedJson, wallet.classicAddress().value());
+            String signature = keyPairService.sign(unsignedBinaryHex, wallet.privateKey()
+              .orElseThrow(() -> new RuntimeException("Wallet must provide a private key to sign the transaction.")));
+            return SignerWrapper.of(Signer.builder()
+              .account(wallet.classicAddress())
+              .signingPublicKey(wallet.publicKey())
+              .transactionSignature(signature)
+              .build()
+            );
+          } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+          }
+        }
+      )
+      .collect(Collectors.toList());
+
+    final AddressCodec addressCodec = AddressCodec.getInstance();
+    signers = signers.stream().sorted(
+      Comparator.comparing(
+        signature -> new BigInteger(addressCodec.decodeAccountId(
+          Address.of(signature.signer().account().value())
+        ).hexValue(), 16),
+        Comparator.reverseOrder()
+      )
+    ).collect(Collectors.toList());
 
     /////////////////////////////
     // Then we add the signatures to the Payment object and submit it

68128b4a480691a94ad7fadd14fd50900ca37ee1
nkramer44 <noah.ph.kramer@gmail.com>
2023-01-19 12:03:22 -0500

    update data-driven-tests to current set from xrpl.js

--- xrpl4j-binary-codec/src/test/java/org/xrpl/xrpl4j/codec/binary/FieldHeaderCodecTest.java
+++ xrpl4j-binary-codec/src/test/java/org/xrpl/xrpl4j/codec/binary/FieldHeaderCodecTest.java
@@ -9,9 +9,9 @@ package org.xrpl.xrpl4j.codec.binary;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,7 +43,7 @@ class FieldHeaderCodecTest {
     ObjectMapper objectMapper = BinaryCodecObjectMapperFactory.getObjectMapper();
     fieldHeaderCodec = new FieldHeaderCodec(new DefaultDefinitionsProvider(objectMapper).get(), objectMapper);
     fieldTests = FixtureUtils.getDataDrivenFixtures().fieldTests();
-    assertThat(fieldTests).hasSize(125);
+    assertThat(fieldTests).hasSize(123);
   }
 
 

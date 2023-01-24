1992def67a5d4297502fdf4ac5c05e4f22ab12e0
nkramer44 <noah.ph.kramer@gmail.com>
2023-01-24 10:18:11 -0500

    improve xrpl4j-core test coverage

--- xrpl4j-core/src/test/java/org/xrpl/xrpl4j/core/ByteUtilsTest.java
+++ xrpl4j-core/src/test/java/org/xrpl/xrpl4j/core/ByteUtilsTest.java
@@ -3,6 +3,7 @@ package org.xrpl.xrpl4j.core;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.google.common.primitives.UnsignedLong;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
@@ -28,6 +29,21 @@ public class ByteUtilsTest {
   }
 
   @Test
+  public void toByteArrayFromBigIntSingleByteMinValue() {
+    assertThat(ByteUtils.toByteArray(BigInteger.ZERO, 1)).isEqualTo(new byte[] {0});
+  }
+
+  @Test
+  public void toByteArrayFromBigIntPadded() {
+    assertThat(ByteUtils.toByteArray(BigInteger.ONE, 2)).isEqualTo(new byte[] {0, 1});
+  }
+
+  @Test
+  public void toByteArrayFromBigIntSingleByteMaxValue() {
+    assertThat(ByteUtils.toByteArray(BigInteger.valueOf(15), 1)[0]).isEqualTo((byte) 15);
+  }
+
+  @Test
   public void checkSizeMaxValue() {
     ByteUtils.checkSize(4, BigInteger.valueOf(15));
   }
@@ -45,4 +61,38 @@ public class ByteUtilsTest {
     ByteUtils.checkSize(4, BigInteger.valueOf(0));
   }
 
+  @Test
+  void toUnsignedLong() {
+    UnsignedByteArray unsignedByteArray = UnsignedByteArray.fromHex("0F1234FF");
+    UnsignedLong unsignedLong = ByteUtils.toUnsignedLong(unsignedByteArray);
+    assertThat(unsignedLong).isEqualTo(UnsignedLong.valueOf(252851455));
+  }
+
+  @Test
+  void paddedWithNoLengthAndEvenNibbles() {
+    String hex = "ABCDEF1234";
+    String padded = ByteUtils.padded(hex);
+    assertThat(padded).isEqualTo(hex);
+  }
+
+  @Test
+  void paddedWithNoLengthAndOddNibbles() {
+    String hex = "ABCDEF123";
+    String padded = ByteUtils.padded(hex);
+    assertThat(padded).isEqualTo("0" + hex);
+  }
+
+  @Test
+  void paddedWithLengthAndEvenNibbles() {
+    String hex = "ABCDEF1234";
+    String padded = ByteUtils.padded(hex, 12);
+    assertThat(padded).isEqualTo("00" + hex);
+  }
+
+  @Test
+  void paddedWithLengthAndOddNibbles() {
+    String hex = "ABCDEF123";
+    String padded = ByteUtils.padded(hex, 12);
+    assertThat(padded).isEqualTo("000" + hex);
+  }
 }

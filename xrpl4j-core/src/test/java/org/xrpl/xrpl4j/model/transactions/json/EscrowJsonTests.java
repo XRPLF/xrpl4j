c0efe1165235671872be467ebddd38bb8067e36c
nkramer44 <noah.ph.kramer@gmail.com>
2023-08-24 07:31:34 -0400

    Fix EscrowFinish condition/fulfillment deserialization (#483)
    * add conditionRawValue and fulfillmentRawValue to EscrowFinish to prevent deserialization failing for EscrowFinishes with malformed conditions/fulfillments
    
    * add conditionRawValue to EscrowCreate to prevent deserialization failing for EscrowCreates with malformed conditions
    
    * log warning
    
    * Revert "add conditionRawValue to EscrowCreate to prevent deserialization failing for EscrowCreates with malformed conditions"
    
    This reverts commit 3262f390daacb141cafbaa97c7f151c937956053.
    
    * add comment

--- xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/transactions/json/EscrowJsonTests.java
+++ xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/transactions/json/EscrowJsonTests.java
@@ -20,14 +20,13 @@ package org.xrpl.xrpl4j.model.transactions.json;
  * =========================LICENSE_END==================================
  */
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.io.BaseEncoding;
 import com.google.common.primitives.UnsignedInteger;
 import com.google.common.primitives.UnsignedLong;
 import com.ripple.cryptoconditions.CryptoConditionReader;
-import com.ripple.cryptoconditions.PreimageSha256Fulfillment;
 import com.ripple.cryptoconditions.der.DerEncodingException;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
@@ -352,19 +351,46 @@ public class EscrowJsonTests extends AbstractJsonTest {
   }
 
   @Test
-  public void testEscrowFinishJsonWithFeeTooLow() {
-    assertThrows(
-      IllegalStateException.class,
-      () -> EscrowFinish.builder()
-        .account(Address.of("rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn"))
-        .fee(XrpCurrencyAmount.ofDrops(3))
-        .sequence(UnsignedInteger.ONE)
-        .owner(Address.of("rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn"))
-        .offerSequence(UnsignedInteger.valueOf(7))
-        .condition(PreimageSha256Fulfillment.from(new byte[48]).getDerivedCondition())
-        .fulfillment(PreimageSha256Fulfillment.from(new byte[60]))
-        .build(),
-      "If a fulfillment is specified, the fee must be set to 330 or greater."
-    );
+  void testEscrowFinishJsonWithMalformedCondition() throws JsonProcessingException {
+    String json = "{\n" +
+      "  \"Account\": \"rKWZ2fDqE5B9XorAcEQZD46H6HEdJQVNdb\",\n" +
+      "  \"Condition\": \"A02580209423ED2EF4CACA8CA4AFC08D3F5EC60A545FD7A97E66E16EA0E2E2\",\n" +
+      "  \"Fee\": \"563\",\n" +
+      "  \"Fulfillment\": \"A02280203377850F1B3A4322F1562DF6F75D584596ABE5B9C76EEA8301F56CB942ACC69B\",\n" +
+      "  \"LastLedgerSequence\": 40562057,\n" +
+      "  \"OfferSequence\": 40403748,\n" +
+      "  \"Owner\": \"r3iocgQwoGNCYyvvt8xuWv2XYXx7Z2gQqd\",\n" +
+      "  \"Sequence\": 39899485,\n" +
+      "  \"SigningPubKey\": \"ED09285829A011D520A1873A0E2F1014F5D6B66A6DDE6953FC02C8185EAFA6A1B0\",\n" +
+      "  \"TransactionType\": \"EscrowFinish\",\n" +
+      "  \"TxnSignature\": \"A3E64F6B8D1D7C4FBC9663FCD217F86C3529EC2E2F16442DD48D1B66EEE30EAC2CE960A76080F74BC749" +
+      "8CA7BBFB822BEE9E8F767114D7B54F7403A7CB672501\"\n" +
+      "}";
+
+    EscrowFinish escrowFinish = objectMapper.readValue(json, EscrowFinish.class);
+    assertThat(escrowFinish.condition()).isEmpty();
+    assertThat(escrowFinish.conditionRawValue()).isNotEmpty().get()
+      .isEqualTo("A02580209423ED2EF4CACA8CA4AFC08D3F5EC60A545FD7A97E66E16EA0E2E2");
+  }
+
+  @Test
+  void testEscrowFinishJsonWithMalformedFulfillment() throws JsonProcessingException {
+    String json = String.format("{\n" +
+      "    \"Account\": \"rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn\",\n" +
+      "    \"TransactionType\": \"EscrowFinish\",\n" +
+      "    \"Owner\": \"rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn\",\n" +
+      "    \"OfferSequence\": 7,\n" +
+      "    \"Condition\": \"A0258020E3B0C44298FC1C149AFBF4C8996FB92427AE41E4649B934CA495991B7852B855810100\",\n" +
+      "    \"Fulfillment\": \"123\",\n" +
+      "    \"Sequence\": 1,\n" +
+      "    \"Flags\": %s,\n" +
+      "    \"SigningPubKey\" : \"02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC\",\n" +
+      "    \"Fee\": \"330\"\n" +
+      "}", TransactionFlags.FULLY_CANONICAL_SIG.getValue());
+
+    EscrowFinish escrowFinish = objectMapper.readValue(json, EscrowFinish.class);
+    assertThat(escrowFinish.fulfillment()).isEmpty();
+    assertThat(escrowFinish.fulfillmentRawValue()).isNotEmpty().get()
+      .isEqualTo("123");
   }
 }

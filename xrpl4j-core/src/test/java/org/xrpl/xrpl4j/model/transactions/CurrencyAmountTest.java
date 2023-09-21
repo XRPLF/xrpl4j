8aa53eb7cd671acb7f44ad14a81214393f6c4ad8
David Fuelling <sappenin@gmail.com>
2023-09-21 12:42:11 -0600

    Update test to cover binary codec

--- xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/transactions/CurrencyAmountTest.java
+++ xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/transactions/CurrencyAmountTest.java
@@ -24,8 +24,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.google.common.io.BaseEncoding;
 import com.google.common.primitives.UnsignedLong;
 import org.junit.jupiter.api.Test;
+import org.xrpl.xrpl4j.codec.addresses.ByteUtils;
+import org.xrpl.xrpl4j.codec.binary.XrplBinaryCodec;
+import org.xrpl.xrpl4j.model.jackson.ObjectMapperFactory;
+
+import java.nio.charset.StandardCharsets;
 
 /**
  * Unit tests for {@link CurrencyAmount}.
@@ -135,7 +142,17 @@ public class CurrencyAmountTest {
   }
 
   /**
-   * Verify that xrpl4j is unaffected by the bug discovered in rippled.
+   * Verify that xrpl4j is unaffected by the bug discovered in rippled. When rippled APIs are provided 3-character
+   * currency codes, those APIs will upper-case the supplied currency values. Only after that normalization will those
+   * APIs then convert to binary. For example, if a request is made to rippled to sign a payload with a currency code of
+   * `UsD`, the API layer will normalize this value to `USD` (i.e., all-caps) before signing. However, tooling (like
+   * xrpl4j) is not forced to do this kind of upper-case normalization. So, it's possible for any tooling (like xrpl4j)
+   * to unintentionally allow issuers to issue mixed-case, 3-character currency codes. However, there's debate in the GH
+   * issue linked below about whether this is a bug in tooling (like xrpl4j), or if this is actually a bug in the
+   * rippled code base (in which case, the normalization functionality should be removed from rippled, and tooling
+   * should do nothing). Contributors to the issue assert the latter -- i.e., it's a bug in rippled and should be
+   * removed from rippled. There is also PR to this effect. Thus, this test ensures that xrpl4j tooling does the correct
+   * thing (i.e., no currency code normalization, either in our Transaction layer or in the binary codec).
    *
    * @see "https://github.com/XRPLF/rippled/issues/4112"
    */
@@ -148,6 +165,54 @@ public class CurrencyAmountTest {
       .build();
 
     assertThat(issuedCurrencyAmount.currency()).isEqualTo("UsD");
+  }
+
+  @Test
+  public void encodeDecodeMixedCaseCurrencyCode() throws JsonProcessingException {
+    currencyTestHelper("Usd");
+    currencyTestHelper("UsD");
+    currencyTestHelper("USD");
+    currencyTestHelper("$GHOST");
+    currencyTestHelper("$ghost");
+    currencyTestHelper("$ghosT");
+  }
+
+  /**
+   * Helper method to test various currencies codes for capitalization.
+   *
+   * @param currencyCode A {@link String} representing a currency code.
+   */
+  private void currencyTestHelper(String currencyCode) throws JsonProcessingException {
+    if (currencyCode.length() > 3) {
+      currencyCode = ByteUtils.padded(
+        BaseEncoding.base16().encode("$GHOST".getBytes(StandardCharsets.US_ASCII)),
+        40 // <-- Non-standard currency codes must be 40 bytes.
+      );
+    }
+
+    final CurrencyAmount issuedCurrencyAmountMixed = IssuedCurrencyAmount.builder()
+      .issuer(Address.of("rPx8CtHbTkjYbQzrwfDxXfPfLHV9nbjYBz"))
+      .currency(currencyCode)
+      .value("100")
+      .build();
+
+    Payment payment = Payment.builder()
+      .account(Address.of("rPx8CtHbTkjYbQzrwfDxXfPfLHV9nbjYBz"))
+      .destination(Address.of("rPx8CtHbTkjYbQzrwfDxXfPfLHV9nbjYBz"))
+      .amount(issuedCurrencyAmountMixed)
+      .fee(XrpCurrencyAmount.of(UnsignedLong.ONE))
+      .build();
+
+    String transactionJson = ObjectMapperFactory.create().writeValueAsString(payment);
+    String transactionBinary = XrplBinaryCodec.getInstance().encode(transactionJson);
+    String decodedTransactionJson = XrplBinaryCodec.getInstance().decode(transactionBinary);
+    Payment decodedPayment = ObjectMapperFactory.create().readValue(decodedTransactionJson, Payment.class);
+
+    final String finalCurrencyCode = currencyCode;
+    decodedPayment.amount().handle(
+      xrpCurrencyAmount -> fail(),
+      issuedCurrencyAmount -> assertThat(issuedCurrencyAmount.currency()).isEqualTo(finalCurrencyCode)
+    );
   }
 
   @Test

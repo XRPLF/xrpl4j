8c6b6bc166ab197f93c79c3e5723b6b30f446821
nkramer44 <noah.ph.kramer@gmail.com>
2023-10-05 13:22:36 -0400

    Update xrpl4j-core/src/main/java/org/xrpl/xrpl4j/codec/addresses/PrivateKeyCodec.java

--- xrpl4j-core/src/main/java/org/xrpl/xrpl4j/codec/addresses/PrivateKeyCodec.java
+++ xrpl4j-core/src/main/java/org/xrpl/xrpl4j/codec/addresses/PrivateKeyCodec.java
@@ -56,7 +56,7 @@ public class PrivateKeyCodec {
   /**
    * Decode a Base58Check encoded XRPL Node Private Key.
    *
-   * @param privateKeyBytes The Base58 encoded public key to be decoded.
+   * @param privateKeyBase58 The Base58 encoded public key to be decoded.
    *
    * @return An {@link UnsignedByteArray} containing the decoded public key.
    *

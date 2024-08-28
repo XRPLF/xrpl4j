5600f8047ada0391f8e89223abfd27158df83212
Angel Gutierrez <agutierrez@ripple.com>
2024-08-28 19:18:47 -0400

    create test for tecTOO_SOON case

--- xrpl4j-integration-tests/src/test/java/org/xrpl/xrpl4j/tests/AccountDeleteIT.java
+++ xrpl4j-integration-tests/src/test/java/org/xrpl/xrpl4j/tests/AccountDeleteIT.java
@@ -25,6 +25,7 @@ import com.google.common.base.Preconditions;
 import com.google.common.primitives.UnsignedInteger;
 import com.google.common.primitives.UnsignedLong;
 import org.awaitility.Durations;
+import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xrpl.xrpl4j.client.JsonRpcClientErrorException;
@@ -43,6 +44,8 @@ import org.xrpl.xrpl4j.model.client.XrplResult;
 import org.xrpl.xrpl4j.model.client.accounts.*;
 import org.xrpl.xrpl4j.model.client.common.LedgerIndex;
 import org.xrpl.xrpl4j.model.client.common.LedgerSpecifier;
+import org.xrpl.xrpl4j.model.client.fees.FeeResult;
+import org.xrpl.xrpl4j.model.client.fees.FeeUtils;
 import org.xrpl.xrpl4j.model.client.ledger.LedgerRequestParams;
 import org.xrpl.xrpl4j.model.client.ledger.LedgerResult;
 import org.xrpl.xrpl4j.model.client.path.RipplePathFindRequestParams;
@@ -71,8 +74,40 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.core.Is.is;
 
+/**
+ * An integration test that submits AccountDelete transactions that handle a successful usage along with
+ * examples of all failure cases.
+ *
+ * @see "https://xrpl.org/accountset.html"
+ */
+class AccountDeleteIT extends AbstractIT {
+    @Test
+    void testAccountDeleteItFailsWith_tecTOO_SOON() throws JsonRpcClientErrorException, JsonProcessingException {
+        // create two accounts, one will be the destination in the tx
+        KeyPair senderAccount = constructRandomAccount();
+        KeyPair receiverAccount = constructRandomAccount();
 
-public abstract class AccountDeleteIT {
+        // get account info the sequence number
+        AccountInfoResult accountInfo = this.scanForResult(
+                () -> this.getValidatedAccountInfo(senderAccount.publicKey().deriveAddress())
+        );
 
+        // create, sign & submit tx
+        AccountDelete accountDelete = AccountDelete.builder()
+                .account(senderAccount.publicKey().deriveAddress())
+                .fee(XrpCurrencyAmount.builder().value(UnsignedLong.valueOf(2000000)).build())
+                .sequence(accountInfo.accountData().sequence())
+                .destination(receiverAccount.publicKey().deriveAddress())
+                .signingPublicKey(senderAccount.publicKey())
+                .build();
 
+        SingleSignedTransaction<AccountDelete> signedAccountDelete = signatureService.sign(
+                senderAccount.privateKey(), accountDelete
+        );
+        SubmitResult<AccountDelete> response = xrplClient.submit(signedAccountDelete);
+
+        // get tecTOO_SOON because sequence # is too high, need to wait for ledger index to be
+        // greater than sequence number + 256
+        assertThat(response.engineResult()).isEqualTo("tecTOO_SOON");
+    }
 }

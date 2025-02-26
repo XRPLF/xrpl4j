16dc3617971bf989c327c50834c4e885bcf6c407
Angel Gutierrez <agutierrez@ripple.com>
2025-02-26 00:01:29 -0500

    address issues in unit test

--- xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/transactions/AmmClawbackTest.java
+++ xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/transactions/AmmClawbackTest.java
@@ -10,21 +10,25 @@ import org.xrpl.xrpl4j.model.flags.AmmClawbackFlags;
 import org.xrpl.xrpl4j.model.flags.TransactionFlags;
 import org.xrpl.xrpl4j.model.ledger.Issue;
 
+/**
+ * Unit tests for {@link AmmClawback}.
+ **/
 public class AmmClawbackTest extends AbstractJsonTest {
 
+  String usd = "USD";
   @Test
   void testJson() throws JSONException, JsonProcessingException {
     AmmClawback ammClawback = AmmClawback.builder()
         .account(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
         .amount(
             IssuedCurrencyAmount.builder()
-                .currency("TST")
+                .currency(usd)
                 .issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd"))
                 .value("25")
                 .build()
         )
-        .asset(Issue.builder().currency("TST").issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
-        .asset2(Issue.builder().currency("TST").issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
+        .asset(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
+        .asset2(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
         .holder(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
         .fee(XrpCurrencyAmount.ofDrops(10))
         .sequence(UnsignedInteger.valueOf(6))
@@ -36,19 +40,20 @@ public class AmmClawbackTest extends AbstractJsonTest {
     String json = "{\n" +
         "    \"Account\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
         "    \"Amount\" : {\n" +
-        "        \"currency\" : \"TST\",\n" +
+        "        \"currency\" : \"USD\",\n" +
         "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\",\n" +
         "        \"value\" : \"25\"\n" +
         "    },\n" +
         "    \"Asset\" : {\n" +
-        "        \"currency\" : \"TST\",\n" +
+        "        \"currency\" : \"USD\",\n" +
         "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
         "    },\n" +
         "    \"Asset2\" : {\n" +
-        "        \"currency\" : \"TST\",\n" +
+        "        \"currency\" : \"USD\",\n" +
         "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
         "    },\n" +
         "    \"Holder\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
+        "    \"Flags\" : 0,\n" +
         "    \"Fee\" : \"10\",\n" +
         "    \"Sequence\" : 6,\n" +
         "    \"SigningPubKey\" : \"02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC\",\n" +
@@ -64,26 +69,36 @@ public class AmmClawbackTest extends AbstractJsonTest {
         .account(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
         .amount(
             IssuedCurrencyAmount.builder()
-                .currency("TST")
+                .currency(usd)
                 .issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd"))
                 .value("25")
                 .build()
         )
         .holder(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
+        .asset(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
+        .asset2(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
         .fee(XrpCurrencyAmount.ofDrops(10))
         .sequence(UnsignedInteger.valueOf(6))
         .signingPublicKey(
             PublicKey.fromBase16EncodedPublicKey("02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC")
         )
-        .flags((AmmClawbackFlags) AmmClawbackFlags.UNSET)
+        .flags(AmmClawbackFlags.UNSET)
         .build();
 
     String json = "{\n" +
         "    \"Account\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
         "    \"Amount\" : {\n" +
-        "        \"currency\" : \"TST\",\n" +
+        "        \"currency\" : \"USD\",\n" +
         "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\",\n" +
         "        \"value\" : \"25\"\n" +
+        "    },\n" +
+        "    \"Asset\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
+        "    },\n" +
+        "    \"Asset2\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
         "    },\n" +
         "    \"Holder\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
         "    \"Flags\" : 0,\n" +
@@ -102,12 +117,14 @@ public class AmmClawbackTest extends AbstractJsonTest {
         .account(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
         .amount(
             IssuedCurrencyAmount.builder()
-                .currency("TST")
+                .currency(usd)
                 .issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd"))
                 .value("25")
                 .build()
         )
         .holder(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
+        .asset(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
+        .asset2(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
         .fee(XrpCurrencyAmount.ofDrops(10))
         .sequence(UnsignedInteger.valueOf(6))
         .signingPublicKey(
@@ -119,12 +136,20 @@ public class AmmClawbackTest extends AbstractJsonTest {
     String json = "{\n" +
         "    \"Account\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
         "    \"Amount\" : {\n" +
-        "        \"currency\" : \"TST\",\n" +
+        "        \"currency\" : \"USD\",\n" +
         "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\",\n" +
         "        \"value\" : \"25\"\n" +
         "    },\n" +
+        "    \"Asset\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
+        "    },\n" +
+        "    \"Asset2\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
+        "    },\n" +
         "    \"Holder\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
-        "    \"Flags\" : 343243252524,\n" +
+        "    \"Flags\" : 1,\n" +
         "    \"Fee\" : \"10\",\n" +
         "    \"Sequence\" : 6,\n" +
         "    \"SigningPubKey\" : \"02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC\",\n" +

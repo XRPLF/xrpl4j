257c468bc60de828319324485840102d462480bd
Angel Gutierrez <agutierrez@ripple.com>
2025-04-08 23:24:58 -0400

    apply formatting to amm clawback test java

--- xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/transactions/AmmClawbackTest.java
+++ xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/transactions/AmmClawbackTest.java
@@ -19,46 +19,46 @@ public class AmmClawbackTest extends AbstractJsonTest {
   @Test
   void testJson() throws JSONException, JsonProcessingException {
     AmmClawback ammClawback = AmmClawback.builder()
-        .account(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
-        .amount(
-            IssuedCurrencyAmount.builder()
-                .currency(usd)
-                .issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd"))
-                .value("25")
-                .build()
-        )
-        .asset(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
-        .asset2(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
-        .holder(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
-        .fee(XrpCurrencyAmount.ofDrops(10))
-        .sequence(UnsignedInteger.valueOf(6))
-        .signingPublicKey(
-            PublicKey.fromBase16EncodedPublicKey("02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC")
-        )
-        .build();
+      .account(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
+      .amount(
+        IssuedCurrencyAmount.builder()
+          .currency(usd)
+          .issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd"))
+          .value("25")
+          .build()
+      )
+      .asset(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
+      .asset2(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
+      .holder(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
+      .fee(XrpCurrencyAmount.ofDrops(10))
+      .sequence(UnsignedInteger.valueOf(6))
+      .signingPublicKey(
+        PublicKey.fromBase16EncodedPublicKey("02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC")
+      )
+      .build();
 
     String json =
-        "{\n" +
-            "    \"Account\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
-            "    \"Amount\" : {\n" +
-            "        \"currency\" : \"USD\",\n" +
-            "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\",\n" +
-            "        \"value\" : \"25\"\n" +
-            "    },\n" +
-            "    \"Asset\" : {\n" +
-            "        \"currency\" : \"USD\",\n" +
-            "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
-            "    },\n" +
-            "    \"Asset2\" : {\n" +
-            "        \"currency\" : \"USD\",\n" +
-            "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
-            "    },\n" +
-            "    \"Holder\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
-            "    \"Fee\" : \"10\",\n" +
-            "    \"Sequence\" : 6,\n" +
-            "    \"SigningPubKey\" : \"02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC\",\n" +
-            "    \"TransactionType\" : \"AMMClawback\"\n" +
-            "}";
+      "{\n" +
+        "    \"Account\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
+        "    \"Amount\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\",\n" +
+        "        \"value\" : \"25\"\n" +
+        "    },\n" +
+        "    \"Asset\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
+        "    },\n" +
+        "    \"Asset2\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
+        "    },\n" +
+        "    \"Holder\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
+        "    \"Fee\" : \"10\",\n" +
+        "    \"Sequence\" : 6,\n" +
+        "    \"SigningPubKey\" : \"02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC\",\n" +
+        "    \"TransactionType\" : \"AMMClawback\"\n" +
+        "}";
 
     assertCanSerializeAndDeserialize(ammClawback, json);
   }
@@ -66,48 +66,48 @@ public class AmmClawbackTest extends AbstractJsonTest {
   @Test
   void testJsonWithUnsetFlags() throws JSONException, JsonProcessingException {
     AmmClawback ammClawback = AmmClawback.builder()
-        .account(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
-        .amount(
-            IssuedCurrencyAmount.builder()
-                .currency(usd)
-                .issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd"))
-                .value("25")
-                .build()
-        )
-        .holder(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
-        .asset(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
-        .asset2(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
-        .fee(XrpCurrencyAmount.ofDrops(10))
-        .sequence(UnsignedInteger.valueOf(6))
-        .signingPublicKey(
-            PublicKey.fromBase16EncodedPublicKey("02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC")
-        )
-        .flags(AmmClawbackFlags.UNSET)
-        .build();
+      .account(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
+      .amount(
+        IssuedCurrencyAmount.builder()
+          .currency(usd)
+          .issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd"))
+          .value("25")
+          .build()
+      )
+      .holder(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
+      .asset(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
+      .asset2(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
+      .fee(XrpCurrencyAmount.ofDrops(10))
+      .sequence(UnsignedInteger.valueOf(6))
+      .signingPublicKey(
+        PublicKey.fromBase16EncodedPublicKey("02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC")
+      )
+      .flags(AmmClawbackFlags.UNSET)
+      .build();
 
     String json =
-        "{\n" +
-            "    \"Account\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
-            "    \"Amount\" : {\n" +
-            "        \"currency\" : \"USD\",\n" +
-            "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\",\n" +
-            "        \"value\" : \"25\"\n" +
-            "    },\n" +
-            "    \"Asset\" : {\n" +
-            "        \"currency\" : \"USD\",\n" +
-            "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
-            "    },\n" +
-            "    \"Asset2\" : {\n" +
-            "        \"currency\" : \"USD\",\n" +
-            "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
-            "    },\n" +
-            "    \"Holder\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
-            "    \"Flags\" : 0,\n" +
-            "    \"Fee\" : \"10\",\n" +
-            "    \"Sequence\" : 6,\n" +
-            "    \"SigningPubKey\" : \"02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC\",\n" +
-            "    \"TransactionType\" : \"AMMClawback\"\n" +
-            "}";
+      "{\n" +
+        "    \"Account\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
+        "    \"Amount\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\",\n" +
+        "        \"value\" : \"25\"\n" +
+        "    },\n" +
+        "    \"Asset\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
+        "    },\n" +
+        "    \"Asset2\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
+        "    },\n" +
+        "    \"Holder\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
+        "    \"Flags\" : 0,\n" +
+        "    \"Fee\" : \"10\",\n" +
+        "    \"Sequence\" : 6,\n" +
+        "    \"SigningPubKey\" : \"02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC\",\n" +
+        "    \"TransactionType\" : \"AMMClawback\"\n" +
+        "}";
 
     assertCanSerializeAndDeserialize(ammClawback, json);
   }
@@ -115,48 +115,48 @@ public class AmmClawbackTest extends AbstractJsonTest {
   @Test
   void testJsonWithNonZeroFlags() throws JSONException, JsonProcessingException {
     AmmClawback ammClawback = AmmClawback.builder()
-        .account(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
-        .amount(
-            IssuedCurrencyAmount.builder()
-                .currency(usd)
-                .issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd"))
-                .value("25")
-                .build()
-        )
-        .holder(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
-        .asset(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
-        .asset2(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
-        .fee(XrpCurrencyAmount.ofDrops(10))
-        .sequence(UnsignedInteger.valueOf(6))
-        .signingPublicKey(
-            PublicKey.fromBase16EncodedPublicKey("02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC")
-        )
-        .flags(AmmClawbackFlags.CLAW_TWO_ASSETS)
-        .build();
+      .account(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
+      .amount(
+        IssuedCurrencyAmount.builder()
+          .currency(usd)
+          .issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd"))
+          .value("25")
+          .build()
+      )
+      .holder(Address.of("rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm"))
+      .asset(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
+      .asset2(Issue.builder().currency(usd).issuer(Address.of("rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd")).build())
+      .fee(XrpCurrencyAmount.ofDrops(10))
+      .sequence(UnsignedInteger.valueOf(6))
+      .signingPublicKey(
+        PublicKey.fromBase16EncodedPublicKey("02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC")
+      )
+      .flags(AmmClawbackFlags.CLAW_TWO_ASSETS)
+      .build();
 
     String json =
-        "{\n" +
-            "    \"Account\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
-            "    \"Amount\" : {\n" +
-            "        \"currency\" : \"USD\",\n" +
-            "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\",\n" +
-            "        \"value\" : \"25\"\n" +
-            "    },\n" +
-            "    \"Asset\" : {\n" +
-            "        \"currency\" : \"USD\",\n" +
-            "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
-            "    },\n" +
-            "    \"Asset2\" : {\n" +
-            "        \"currency\" : \"USD\",\n" +
-            "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
-            "    },\n" +
-            "    \"Holder\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
-            "    \"Flags\" : 1,\n" +
-            "    \"Fee\" : \"10\",\n" +
-            "    \"Sequence\" : 6,\n" +
-            "    \"SigningPubKey\" : \"02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC\",\n" +
-            "    \"TransactionType\" : \"AMMClawback\"\n" +
-            "}";
+      "{\n" +
+        "    \"Account\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
+        "    \"Amount\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\",\n" +
+        "        \"value\" : \"25\"\n" +
+        "    },\n" +
+        "    \"Asset\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
+        "    },\n" +
+        "    \"Asset2\" : {\n" +
+        "        \"currency\" : \"USD\",\n" +
+        "        \"issuer\" : \"rP9jPyP5kyvFRb6ZiRghAGw5u8SGAmU4bd\"\n" +
+        "    },\n" +
+        "    \"Holder\" : \"rJVUeRqDFNs2xqA7ncVE6ZoAhPUoaJJSQm\",\n" +
+        "    \"Flags\" : 1,\n" +
+        "    \"Fee\" : \"10\",\n" +
+        "    \"Sequence\" : 6,\n" +
+        "    \"SigningPubKey\" : \"02356E89059A75438887F9FEE2056A2890DB82A68353BE9C0C0C8F89C0018B37FC\",\n" +
+        "    \"TransactionType\" : \"AMMClawback\"\n" +
+        "}";
 
     assertCanSerializeAndDeserialize(ammClawback, json);
   }

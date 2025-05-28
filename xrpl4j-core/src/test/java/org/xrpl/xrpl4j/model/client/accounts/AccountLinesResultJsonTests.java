c62cce62960124310e9aa7ec529c661d58b3a4dc
nkramer44 <noah.ph.kramer@gmail.com>
2025-05-28 15:57:41 -0400

    Add support for deep freeze (#620)
    * Add support for the Deep Freeze amendment (https://github.com/XRPLF/XRPL-Standards/tree/master/XLS-0077-deep-freeze)

--- xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/client/accounts/AccountLinesResultJsonTests.java
+++ xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/client/accounts/AccountLinesResultJsonTests.java
@@ -9,9 +9,9 @@ package org.xrpl.xrpl4j.model.client.accounts;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -75,6 +75,8 @@ public class AccountLinesResultJsonTests extends AbstractJsonTest {
       "                \"peer_authorized\": false,\n" +
       "                \"freeze\": false,\n" +
       "                \"freeze_peer\": false,\n" +
+      "                \"deep_freeze\": false,\n" +
+      "                \"deep_freeze_peer\": false,\n" +
       "                \"quality_in\": 0,\n" +
       "                \"quality_out\": 0\n" +
       "            },\n" +
@@ -90,6 +92,8 @@ public class AccountLinesResultJsonTests extends AbstractJsonTest {
       "                \"peer_authorized\": false,\n" +
       "                \"freeze\": false,\n" +
       "                \"freeze_peer\": false,\n" +
+      "                \"deep_freeze\": false,\n" +
+      "                \"deep_freeze_peer\": false,\n" +
       "                \"quality_in\": 0,\n" +
       "                \"quality_out\": 0\n" +
       "            }\n" +

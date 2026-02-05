c4f558fa6208ce7a74f6fb378cf1cb8ba44ee953
David Fuelling <sappenin@gmail.com>
2026-02-05 15:44:41 -0700

    Fix compilation warnings for Immutables (#664)
    * Add equals/hashcode to XrpCurrencyAmount, MpTokenIssuanceId
    * Fix equals/hashcode in CredentialType, CredentialUri, NfTokenId, NfTokenUri
    * Add unit test coverage to Wrapper.java

--- xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/transactions/Hash256Test.java
+++ xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/transactions/Hash256Test.java
@@ -9,9 +9,9 @@ package org.xrpl.xrpl4j.model.transactions;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,40 +21,81 @@ package org.xrpl.xrpl4j.model.transactions;
  */
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.immutables.value.Value;
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.xrpl.xrpl4j.model.jackson.ObjectMapperFactory;
 
 /**
  * Unit tests for {@link Hash256}.
  */
 public class Hash256Test {
 
+  private final ObjectMapper objectMapper = ObjectMapperFactory.create();
+
+  private static final Hash256 HASH_256_MAX = Hash256.of(
+    "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+  private static final Hash256 HASH_256_ZERO = Hash256.of(
+    "0000000000000000000000000000000000000000000000000000000000000000");
+
   @Test
-  public void hashEquality() {
-    assertThat(Hash256.of("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"))
-      .isEqualTo(Hash256.of("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"));
-
-    assertThat(Hash256.of("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"))
-      .isEqualTo(Hash256.of("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
-
-    assertThat(Hash256.of("0000000000000000000000000000000000000000000000000000000000000000"))
-      .isNotEqualTo(Hash256.of("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"));
-
-    assertThat(Hash256.of("0000000000000000000000000000000000000000000000000000000000000000")).isNotEqualTo(null);
-    assertThat(Hash256.of("0000000000000000000000000000000000000000000000000000000000000000"))
-      .isNotEqualTo(new Object());
+  void hashEquality() {
+    assertThrows(NullPointerException.class, () -> Hash256.of(null));
+    assertThat(HASH_256_MAX).isEqualTo(HASH_256_MAX);
+    assertThat(HASH_256_MAX).isEqualTo(Hash256.of("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+    assertThat(HASH_256_ZERO).isNotEqualTo(HASH_256_MAX);
+    assertThat(HASH_256_ZERO.equals(null)).isFalse();
+    assertThat(HASH_256_ZERO).isNotEqualTo(new Object());
   }
 
   @Test
-  public void hashHashcode() {
-    assertThat(Hash256.of("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF").hashCode())
-      .isEqualTo(Hash256.of("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF").hashCode());
+  void hashHashcode() {
+    assertThat(HASH_256_MAX.hashCode()).isEqualTo(HASH_256_MAX.hashCode());
 
-    assertThat(Hash256.of("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF").hashCode())
-      .isEqualTo(Hash256.of("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff").hashCode());
+    assertThat(HASH_256_MAX.hashCode()).isEqualTo(
+      Hash256.of("ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff").hashCode());
 
-    assertThat(Hash256.of("0000000000000000000000000000000000000000000000000000000000000000").hashCode())
-      .isNotEqualTo(Hash256.of("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF").hashCode());
+    assertThat(HASH_256_ZERO.hashCode()).isNotEqualTo(HASH_256_MAX.hashCode());
+  }
+
+  @Test
+  void testToString() {
+    assertThat(HASH_256_ZERO.toString()).isEqualTo("0000000000000000000000000000000000000000000000000000000000000000");
+  }
+
+  @Test
+  void testJsonSerialization() throws JsonProcessingException, JSONException {
+    Hash256 hash = Hash256.of("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+    Hash256Wrapper wrapper = ImmutableHash256Wrapper.builder()
+      .hash(hash)
+      .build();
+
+    String json = "{\"hash\":\"FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF\"}";
+    assertSerializesAndDeserializes(wrapper, json);
+  }
+
+  private void assertSerializesAndDeserializes(Hash256Wrapper wrapper, String json)
+    throws JsonProcessingException, JSONException {
+    String serialized = objectMapper.writeValueAsString(wrapper);
+    JSONAssert.assertEquals(json, serialized, JSONCompareMode.STRICT);
+    Hash256Wrapper deserialized = objectMapper.readValue(serialized, Hash256Wrapper.class);
+    assertThat(deserialized).isEqualTo(wrapper);
+  }
+
+  @Value.Immutable
+  @JsonSerialize(as = ImmutableHash256Wrapper.class)
+  @JsonDeserialize(as = ImmutableHash256Wrapper.class)
+  interface Hash256Wrapper {
+
+    Hash256 hash();
   }
 
 }

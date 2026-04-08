aaa8cdc72cbb5507ee10ced24f207f7c8246ab65
Raj Patel <rajp@ripple.com>
2026-04-08 09:44:13 -0400

    remove mpt-crypto source

--- xrpl4j-core/src/main/java/org/xrpl/xrpl4j/crypto/confidential/mpt-crypto/tests/test_bulletproof_verify_external.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include "test_utils.h"
-
-/* Placeholder - verification is done within test_bulletproof_vectors.c */
-int main() {
-    printf("Bulletproof verify external: PASSED (verification done in vector generator)\n");
-    return 0;
-}

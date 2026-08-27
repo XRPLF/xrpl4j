1a964ed5486f68d544ec7505481875735e95ad59
nkramer44 <noah.ph.kramer@gmail.com>
2026-08-27 12:46:20 -0400

    Fix json serialization of UInt192Type (#828)
    * fix json serialization of UInt192Type.
    
    Signed-off-by: nkramer44 <noah.ph.kramer@gmail.com>
    
    * make UInt192Type a HashType
    
    Signed-off-by: nkramer44 <noah.ph.kramer@gmail.com>
    
    * Revert "make UInt192Type a HashType"
    
    This reverts commit 39e485a4d15708adc1e39ac37e380daf991741c4.
    
    * fix test
    
    Signed-off-by: nkramer44 <noah.ph.kramer@gmail.com>
    
    * Fix checkstyle line-length violations in UInt192 tests
    
    Signed-off-by: nkramer44 <noah.ph.kramer@gmail.com>
    
    * fix indent
    
    ---------
    
    Signed-off-by: nkramer44 <noah.ph.kramer@gmail.com>

---
+++ xrpl4j-core/src/test/java/org/xrpl/xrpl4j/codec/binary/types/UInt192TypeTest.java
@@ -0,0 +1,48 @@
+package org.xrpl.xrpl4j.codec.binary.types;
+
+/*-
+ * ========================LICENSE_START=================================
+ * xrpl4j :: core
+ * %%
+ * Copyright (C) 2020 - 2023 XRPL Foundation and its contributors
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =========================LICENSE_END==================================
+ */
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.fasterxml.jackson.databind.node.TextNode;
+import org.junit.jupiter.api.Test;
+
+public class UInt192TypeTest {
+
+  private final UInt192Type codec = new UInt192Type();
+
+  @Test
+  void decode() {
+    assertThat(codec.fromHex("000000000000000000000000000000000000000000000000").toHex())
+      .isEqualTo("000000000000000000000000000000000000000000000000");
+    assertThat(codec.fromHex("00000000000000000000000000000000000000000000000F").toHex())
+      .isEqualTo("00000000000000000000000000000000000000000000000F");
+    assertThat(codec.fromHex("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF").toHex())
+      .isEqualTo("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF");
+  }
+
+  @Test
+  void encode() {
+    assertThat(codec.fromJson(new TextNode("013411307C0D97D1EC6B2989138679ACDCB75C37CA30F0A6")).toHex())
+      .isEqualTo("013411307C0D97D1EC6B2989138679ACDCB75C37CA30F0A6");
+  }
+
+}

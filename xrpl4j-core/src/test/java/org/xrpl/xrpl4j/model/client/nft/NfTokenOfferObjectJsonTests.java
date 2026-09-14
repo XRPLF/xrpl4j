1fe7ba34f891a696f547910e9ee9e218492ebe1b
David Fuelling <sappenin@gmail.com>
2026-09-14 15:16:11 -0600

    Remove errant import
    Signed-off-by: David Fuelling <sappenin@gmail.com>

--- xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/client/nft/NfTokenOfferObjectJsonTests.java
+++ xrpl4j-core/src/test/java/org/xrpl/xrpl4j/model/client/nft/NfTokenOfferObjectJsonTests.java
@@ -22,7 +22,6 @@ package org.xrpl.xrpl4j.model.client.nft;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.google.common.primitives.UnsignedInteger;
-import com.google.common.primitives.UnsignedLong;
 import org.json.JSONException;
 import org.junit.jupiter.api.Test;
 import org.xrpl.xrpl4j.model.AbstractJsonTest;

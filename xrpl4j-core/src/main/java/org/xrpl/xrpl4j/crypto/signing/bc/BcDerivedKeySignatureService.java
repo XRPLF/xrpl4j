ca03de217f49dcbdc3666a44469702e417a0bb2b
David Fuelling <sappenin@gmail.com>
2026-09-17 13:48:03 -0600

    Fix formatting
    Signed-off-by: David Fuelling <sappenin@gmail.com>

--- xrpl4j-core/src/main/java/org/xrpl/xrpl4j/crypto/signing/bc/BcDerivedKeySignatureService.java
+++ xrpl4j-core/src/main/java/org/xrpl/xrpl4j/crypto/signing/bc/BcDerivedKeySignatureService.java
@@ -324,8 +324,10 @@ public class BcDerivedKeySignatureService implements SignatureService<PrivateKey
     byte[] passphraseBytes = EMPTY_BYTE_ARRAY; // <-- to avoid an NPE in the "finally" block.
     try {
       passphraseBytes = Hashing.hmacSha512(
-          serverSecretBytes) // <-- This is equivalent to the `passphraseBytes` in the xrpl.org docs.
-        .hashBytes(accountIdentifier.getBytes()).asBytes();
+          serverSecretBytes // <-- This is equivalent to the `passphraseBytes` in the xrpl.org docs.
+        )
+        .hashBytes(accountIdentifier.getBytes())
+        .asBytes();
       return Seed.secp256k1SeedFromPassphrase(Passphrase.of(passphraseBytes));
     } finally {
       // Zero-out all bytes in both local arrays so secret material exists in-memory for as little time as possible.
